5fc77de8ee3a6e443191fe3c4897153bddbab55c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-10-15 12:03:16 +0200

    corrected typos in gen subs

--- jean_zay/submitit/fastmri_reproducible_benchmark/general_submissions.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/general_submissions.py
@@ -55,9 +55,9 @@ def eval_grid(run_ids, job_name, eval_function, parameter_grid, n_samples=50):
         for run_id, param in zip(run_ids, parameters):
             job = executor.submit(eval_function, run_id=run_id, n_samples=n_samples, **param)
             jobs.append(job)
-    for param, original_param, job in zip(parameters, original_parameters, jobs)
+    for param, original_param, job in zip(parameters, original_parameters, jobs):
         metrics_names, eval_res = job.result()
         param.update(original_param)
-        print('Parameters', params)
+        print('Parameters', param)
         print(metrics_names)
         print(eval_res)
