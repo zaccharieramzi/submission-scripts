e9c9237be66806856c8587469a120e3092ca6de7
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-10-15 11:55:35 +0200

    added setup to allow this to be a package

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# taken from https://github.com/CEA-COSMIC/ModOpt/blob/master/setup.py
+with open('requirements.txt') as open_file:
+    install_requires = open_file.read()
+
+setuptools.setup(
+    name="zac-ramzi-sub-scripts",
+    version="0.0.1",
+    author="Zaccharie Ramzi",
+    author_email="zaccharie.ramzi@gmail.com",
+    description="Submission scripts for JZ and TGCC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zaccharieramzi/submission-scripts",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+    python_requires='>=3.6',
+)
