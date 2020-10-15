37340a03a59a96b9c82a80b9fa5013078b687ebf
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-10-15 17:45:42 +0200

    made general submission more configurable

--- jean_zay/submitit/fastmri_reproducible_benchmark/general_submissions.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/general_submissions.py
@@ -29,9 +29,23 @@ def get_executor(job_name, timeout_hour=60, n_gpus=1):
     )
     return executor
 
-def train_eval_grid(job_name, train_function, eval_function, parameter_grid, n_samples_eval=50):
-    parameters = list(ParameterGrid(parameter_grid))
-    executor = get_executor(job_name, timeout_hour=60, n_gpus=4)
+def train_eval_grid(
+        job_name,
+        train_function,
+        eval_function,
+        parameter_grid,
+        n_samples_eval=50,
+        to_grid=True,
+        timeout_train=60,
+        n_gpus_train=4,
+        timeout_eval=10,
+        n_gpus_eval=4,
+    ):
+    if to_grid:
+        parameters = list(ParameterGrid(parameter_grid))
+    else:
+        parameters = parameter_grid
+    executor = get_executor(job_name, timeout_hour=timeout_train, n_gpus=n_gpus_train)
     jobs = []
     with executor.batch():
         for param in parameters:
@@ -39,11 +53,32 @@ def train_eval_grid(job_name, train_function, eval_function, parameter_grid, n_s
             jobs.append(job)
     run_ids = [job.result() for job in jobs]
     print(run_ids)
-    eval_grid(run_ids, job_name, eval_function, parameter_grid, n_samples=n_samples_eval)
+    eval_grid(
+        run_ids,
+        job_name,
+        eval_function,
+        parameter_grid,
+        n_samples=n_samples_eval,
+        to_grid=to_grid,
+        timeout=timeout_eval,
+        n_gpus=n_gpus_eval,
+    )
 
-def eval_grid(run_ids, job_name, eval_function, parameter_grid, n_samples=50):
-    parameters = list(ParameterGrid(parameter_grid))
-    executor = get_executor(job_name, timeout_hour=10, n_gpus=4)
+def eval_grid(
+        run_ids,
+        job_name,
+        eval_function,
+        parameter_grid,
+        n_samples=50,
+        to_grid=True,
+        timeout=10,
+        n_gpus=4,
+    ):
+    if to_grid:
+        parameters = list(ParameterGrid(parameter_grid))
+    else:
+        parameters = parameter_grid
+    executor = get_executor(job_name, timeout_hour=timeout, n_gpus=n_gpus)
     original_parameters = []
     for params in parameters:
         original_params = {}
@@ -55,9 +90,12 @@ def eval_grid(run_ids, job_name, eval_function, parameter_grid, n_samples=50):
         for run_id, param in zip(run_ids, parameters):
             job = executor.submit(eval_function, run_id=run_id, n_samples=n_samples, **param)
             jobs.append(job)
+    eval_results = []
     for param, original_param, job in zip(parameters, original_parameters, jobs):
         metrics_names, eval_res = job.result()
         param.update(original_param)
         print('Parameters', param)
         print(metrics_names)
         print(eval_res)
+        eval_results.append(eval_res)
+    return eval_results
