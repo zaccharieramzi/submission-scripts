84aebd9ae651bdfb0f5763ff64be7a10122dbfca
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-10-21 16:28:42 +0200

    reduced number of iterations in 3d ncpdnet sub

--- jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/three_d.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/three_d.py
@@ -9,7 +9,7 @@ base_params = {
     'n_epochs': [70],
     'af': [4],
     'n_filters': [4],
-    'n_iter': [8],
+    'n_iter': [4],
     'acq_type': ['radial_stacks', 'spiral_stacks'],
     'loss': ['compound_mssim'],
 }
