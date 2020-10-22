9de48842fde35568c382203a3690465a4bec1b5d
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-10-22 16:16:16 +0200

    evaluation mode the real equi training

--- jean_zay/submitit/fastmri_reproducible_benchmark/xpdnet_multicoil/real_equidistant_training.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/xpdnet_multicoil/real_equidistant_training.py
@@ -53,16 +53,25 @@ parameter_grid = [
     ) for _, model_size, model_fun, kwargs, _, n_scales, res in model_specs
 ]
 
-eval_results = train_eval_grid(
+# eval_results = train_eval_grid(
+run_ids = [
+    'xpdnet_sense__af4_CORPD_FBK_compound_mssim_MWCNNmedium_1603186070',
+    'xpdnet_sense__af8_CORPD_FBK_compound_mssim_MWCNNmedium_1603186057',
+]
+eval_results = eval_grid(
+    run_ids,
     job_name,
-    train_xpdnet,
+    # train_xpdnet,
     evaluate_xpdnet,
     parameter_grid,
-    n_samples_eval=100,
-    timeout_train=60,
-    n_gpus_train=1,
-    timeout_eval=10,
-    n_gpus_eval=1,
+    # n_samples_eval=100,
+    # timeout_train=60,
+    # n_gpus_train=1,
+    # timeout_eval=10,
+    # n_gpus_eval=1,
+    n_samples=100,
+    timeout=10,
+    n_gpus=1,
     to_grid=False,
 )
 
