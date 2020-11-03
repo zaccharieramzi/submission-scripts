3a1f3ca229da190721eea8c3ae4a914170497aae
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-03 12:04:19 +0100

    corrected denoising sub script

--- jean_zay/submitit/denoising_vs_reconstruction/denoise.py
+++ jean_zay/submitit/denoising_vs_reconstruction/denoise.py
@@ -5,7 +5,7 @@ from denoising_recon.models.denoisers.proposed_params import get_model_specs
 from denoising_recon.training.train import train
 import pandas as pd
 
-from jean_zay.submitit.fastmri_reproducible_benchmark.general_submissions import train_eval_grid, eval_grid
+from jean_zay.submitit.general_submissions import train_eval_grid, eval_grid
 
 
 job_name = 'denoising_celeba'
@@ -21,7 +21,7 @@ if not grey:
     additional_info += '_color'
 if loss != 'mse':
     additional_info += f'_{loss}'
-model_specs = list(get_model_specs(force_res=True))
+model_specs = list(get_model_specs(grey=grey))
 if model_name is not None:
     model_specs = [ms for ms in model_specs if ms[0] == model_name]
 if model_size is not None:
@@ -30,7 +30,7 @@ if model_size is not None:
 
 parameter_grid = [
     dict(
-        is_denoising=False,
+        is_denoising=True,
         model_fun=model_fun,
         model_kwargs=kwargs,
         grey=grey,
@@ -38,8 +38,9 @@ parameter_grid = [
         n_epochs=n_epochs,
         n_samples=n_samples,
         loss=loss,
-        noise_std=noise_std,
-    ) for model_name, model_size, model_fun, kwargs in model_specs
+        noise_power_spec=noise_std,
+        target_size=(128, 128),
+    ) for model_name, model_size, model_fun, kwargs, _ in model_specs
 ]
 
 eval_results = train_eval_grid(
@@ -48,21 +49,21 @@ eval_results = train_eval_grid(
     train,
     evaluate,
     parameter_grid,
-    n_samples_eval=500,
+    n_samples_eval=5,
     timeout_train=20,
     n_gpus_train=1,
     timeout_eval=4,
     n_gpus_eval=1,
     # n_samples=200,
     # timeout=10,
-    n_gpus=1,
+    # n_gpus=1,
     to_grid=False,
-    noise_std=1,  # just for eval
+    noise_power_spec=1,  # just for eval
 )
 
 df_results = pd.DataFrame(columns='model_name model_size psnr ssim'.split())
 
-for (name, model_size, _, _, _, _, _), eval_res in zip(model_specs, eval_results):
+for (name, model_size, _, _, _), eval_res in zip(model_specs, eval_results):
     df_results = df_results.append(dict(
         model_name=name,
         model_size=model_size,
