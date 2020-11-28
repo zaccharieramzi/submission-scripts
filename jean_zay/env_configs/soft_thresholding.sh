ebaf88f9ffddf839d13e88513594ed1a65027d27
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-28 16:53:07 +0100

    corrected the env var for tf st

--- jean_zay/env_configs/soft_thresholding.sh
+++ jean_zay/env_configs/soft_thresholding.sh
@@ -3,6 +3,6 @@ module purge
 module load python/3.7.5 cuda/10.1.2 cudnn/7.6.5.32-cuda-10.1 nccl/2.5.6-2-cuda
 conda activate soft-thresholding
 
-export BSD_DATA_DIR=$SCRATCH/
+export BSD_DATA_DIR=$SCRATCH/BSR
 export LOGS_DIR=$SCRATCH/
 export CHECKPOINTS_DIR=$SCRATCH/
