95ff390fc5f97290c1aa610f957a67696568c544
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-12-01 14:52:42 +0100

    added a sota reproduction sub script for dncnn

---
+++ jean_zay/submitit/tf_soft_thresholding/dncnn_sota.py
@@ -0,0 +1,66 @@
+from tf_soft_thresholding.training.train_denoisers import train_dncnn as train
+from tf_soft_thresholding.evaluate.evaluate_denoisers import evaluate_dncnn as evaluate
+
+from jean_zay.submitit.general_submissions import train_eval_grid, eval_grid
+
+
+job_name = 'dncnn_sota'
+n_epochs = 1000
+to_grey = True
+patch_size = 50
+patch_size_eval = None
+batch_size = 128
+batch_size_eval = 1
+noise_config = dict(
+    noise_input=True,
+    noise_power_spec=55/255,
+    noise_range_type='uniform',
+)
+noise_config_eval = dict(
+    noise_input=True,
+    fixed_noise=True,
+    noise_power_spec=25/255,
+)
+n_samples_eval = 100
+n_filters = 64
+n_convs = 17
+models = {
+    'dncnn-relu': {},
+    'dcnnn-relu-bn': {'bn': True},
+}
+parameter_grid = [
+    dict(
+        model_config=dict(n_filters=n_filters, n_convs=n_convs, **model_config),
+        model_name=model_name,
+        n_epochs=n_epochs,
+        to_grey=to_grey,
+        patch_size=patch_size,
+        batch_size=batch_size,
+        noise_config=noise_config,
+    )
+    for model_name, model_config in models.items()
+]
+
+eval_results, run_ids = train_eval_grid(
+# eval_results = eval_grid(
+    job_name,
+    train,
+    evaluate,
+    parameter_grid,
+    n_samples_eval=n_samples_eval,
+    timeout_train=20,
+    n_gpus_train=4,
+    timeout_eval=4,
+    n_gpus_eval=1,
+    # n_samples=200,
+    # timeout=10,
+    # n_gpus=1,
+    to_grid=False,
+    patch_size=patch_size_eval,  # just for eval
+    batch_size=batch_size_eval,  # just for eval
+    noise_config=noise_config_eval,  # just for eval
+    project='soft_thresholding',
+    return_run_ids=True,
+)
+
+print(eval_results)
