2b132c61ff3873b5a2084613ce48658def169124
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-12-02 18:35:35 +0100

    corrected learnlets env config

--- jean_zay/env_configs/learnlets.sh
+++ jean_zay/env_configs/learnlets.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 module purge
-module load tensorflow-gpu/py3/2.2.0-dev
+module load python/3.7.5 cuda/10.1.2 cudnn/7.6.5.32-cuda-10.1 nccl/2.5.6-2-cuda
+conda activate learnlets
 
 export BSD500_DATA_DIR=$SCRATCH/
 export BSD68_DATA_DIR=$SCRATCH/
