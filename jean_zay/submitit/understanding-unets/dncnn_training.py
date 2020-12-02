7880f8daf5199b2f445042b05211e403f40e2d6b
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-12-02 19:53:52 +0100

    added dncnn train sub script

---
+++ jean_zay/submitit/understanding-unets/dncnn_training.py
@@ -0,0 +1,31 @@
+from learning_wavelets.training_scripts.dncnn_training import train_dncnn as train
+from learning_wavelets.evaluate_scripts.evaluate_dncnn import evaluate_dncnn as evaluate
+
+from jean_zay.submitit.general_submissions import train_eval_grid, eval_grid
+
+
+job_name = 'dncnn'
+parameter_grid = {
+    'bn': [True, False],
+}
+
+eval_results = train_eval_grid(
+# eval_results = eval_grid(
+    job_name,
+    train,
+    evaluate,
+    parameter_grid,
+    n_samples_eval=None,
+    timeout_train=20,
+    n_gpus_train=4,
+    timeout_eval=4,
+    n_gpus_eval=1,
+    # n_samples=200,
+    # timeout=10,
+    # n_gpus=1,
+    to_grid=True,
+    noise_stds=[0.00001, 5, 15, 20, 25, 30, 50, 55, 60, 75],
+    project='learnlets',
+)
+
+print(eval_results)
