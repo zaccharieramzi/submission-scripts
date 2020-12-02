4bac4861d109a2c442631e28fe2c31fa5fe32b37
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-12-02 19:21:19 +0100

    corrected bsd68 env config var name

--- jean_zay/env_configs/soft_thresholding.sh
+++ jean_zay/env_configs/soft_thresholding.sh
@@ -4,6 +4,6 @@ module load python/3.7.5 cuda/10.1.2 cudnn/7.6.5.32-cuda-10.1 nccl/2.5.6-2-cuda
 conda activate soft-thresholding
 
 export BSD_DATA_DIR=$SCRATCH/BSR
-export BSD_DATA_DIR=$SCRATCH/BSD68
+export BSD68_DATA_DIR=$SCRATCH/BSD68
 export LOGS_DIR=$SCRATCH/
 export CHECKPOINTS_DIR=$SCRATCH/
