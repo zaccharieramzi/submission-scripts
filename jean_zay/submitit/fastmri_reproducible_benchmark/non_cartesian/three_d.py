3864894032c6e29d7d8a6e016aec115ac942b44b
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-12-04 11:28:57 +0100

    updated 3d tf script

--- jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/three_d.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/three_d.py
@@ -6,13 +6,14 @@ from jean_zay.submitit.general_submissions import train_eval_grid, eval_grid
 
 base_params = {
     'three_d': [True],
-    'n_epochs': [15],
-    'af': [4],
-    'n_filters': [4],
+    'n_epochs': [8],
+    'af': [5],
+    'n_filters': [32],
     'n_iter': [4],
     'n_primal': [2],
     'acq_type': ['radial_stacks', 'spiral_stacks'],
     'loss': ['mse'],
+    'use_mixed_precision': [True],
 }
 
 params = [
@@ -20,4 +21,15 @@ params = [
   dict(dcomp=[False], normalize_image=[True], model=['pdnet'], **base_params),
 ]
 
-train_eval_grid('3d_nc', train_ncnet_multinet, evaluate_nc_multinet, params, n_samples_eval=30)
+eval_results = train_eval_grid(
+    '3d_nc',
+    train_ncnet_multinet,
+    evaluate_nc_multinet,
+    params,
+    n_gpus_train=1,
+    timeout_train=100,
+    n_gpus_eval=1,
+    n_samples_eval=30,
+    params_to_ignore=['use_mixed_precision'],
+)
+print(eval_results)
