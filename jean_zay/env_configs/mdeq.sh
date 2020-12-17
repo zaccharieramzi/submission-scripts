6a6611e490874b41d358fd0dfecbea8f9398f9ce
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-12-17 10:09:38 +0100

    added mdeq config

---
+++ jean_zay/env_configs/mdeq.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+module purge
+module load python/3.7.5 cuda/10.1.2 cudnn/7.6.5.32-cuda-10.1 nccl/2.5.6-2-cuda
+conda activate mdeq
+
+export FASTMRI_DATA_DIR=$SCRATCH/
+export LOGS_DIR=$SCRATCH/
+export CHECKPOINTS_DIR=$SCRATCH/
