abf9c1fde06b470cad7fadacf1f174b9243dcc9e
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-01-06 12:17:01 +0100

    added env vars to mdeq config

--- jean_zay/env_configs/mdeq.sh
+++ jean_zay/env_configs/mdeq.sh
@@ -4,5 +4,7 @@ module load python/3.7.5 cuda/10.1.2 cudnn/7.6.5.32-cuda-10.1 nccl/2.5.6-2-cuda
 conda activate mdeq
 
 export FASTMRI_DATA_DIR=$SCRATCH/
-export LOGS_DIR=$SCRATCH/
-export CHECKPOINTS_DIR=$SCRATCH/
+export MDEQ_LOGS=$SCRATCH/logs
+export MDEQ_CHECKPOINTS=$SCRATCH/checkpoints
+export MDEQ_DATA=$SCRATCH/
+export MDEQ_CONFIG=$WORK/mdeq/experiments/
