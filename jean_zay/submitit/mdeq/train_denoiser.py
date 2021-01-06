2e4276b3fdc021efe0021879f97f3046223a6964
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-01-06 13:48:31 +0100

    added 3 other jobs to test quality of denoising with/out bn and res

--- jean_zay/submitit/mdeq/train_denoiser.py
+++ jean_zay/submitit/mdeq/train_denoiser.py
@@ -6,7 +6,12 @@ from jean_zay.submitit.general_submissions import get_executor
 job_name = 'mdeq_denoise'
 executor = get_executor(job_name, timeout_hour=20, n_gpus=1, project='mdeq')
 
-executor.submit(
-    train_mdeq_denoising,
-    n_val=20,
-)
+with executor.batch():
+    for use_bn in [True, False]:
+        for use_res in [True, False]:
+            executor.submit(
+                train_mdeq_denoising,
+                n_val=20,
+                use_res=use_res,
+                use_bn=use_bn,
+            )
