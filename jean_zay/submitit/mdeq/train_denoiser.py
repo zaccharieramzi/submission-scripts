248fb7df85032c180137d273670bb806f694ca5e
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-01-08 17:29:28 +0100

    switched to large network for mdeq training

--- jean_zay/submitit/mdeq/train_denoiser.py
+++ jean_zay/submitit/mdeq/train_denoiser.py
@@ -4,14 +4,15 @@ from jean_zay.submitit.general_submissions import get_executor
 
 
 job_name = 'mdeq_denoise'
-executor = get_executor(job_name, timeout_hour=20, n_gpus=1, project='mdeq')
+executor = get_executor(job_name, timeout_hour=60, n_gpus=1, project='mdeq')
 
 with executor.batch():
-    for use_bn in [True, False]:
-        for use_res in [True, False]:
+    for use_bn in [True]:
+        for use_res in [True]:
             executor.submit(
                 train_mdeq_denoising,
                 n_val=20,
                 use_res=use_res,
                 use_bn=use_bn,
+                network_size='LARGE',
             )
