c06c0ffc40f3795c991d93c9a41d445b9de8c2a9
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-01-08 19:45:52 +0100

    added large denoising

---
+++ jean_zay/submitit/mdeq/train_denoiser_large.py
@@ -0,0 +1,18 @@
+from mdeq_lib.training_scripts.denoise_train import train_mdeq_denoising
+
+from jean_zay.submitit.general_submissions import get_executor
+
+
+job_name = 'mdeq_denoise'
+executor = get_executor(job_name, timeout_hour=60, n_gpus=1, project='mdeq')
+
+with executor.batch():
+    for use_bn in [True]:
+        for use_res in [True]:
+            executor.submit(
+                train_mdeq_denoising,
+                n_val=20,
+                use_res=use_res,
+                use_bn=use_bn,
+                network_size='LARGE',
+            )
