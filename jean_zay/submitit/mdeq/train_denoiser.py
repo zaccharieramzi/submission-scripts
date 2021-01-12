279cf8dd7f170a3666f04bbf23de691c90b1c1a1
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-01-12 16:31:50 +0100

    added new residual and higher gradient clipping to training mdeq

--- jean_zay/submitit/mdeq/train_denoiser.py
+++ jean_zay/submitit/mdeq/train_denoiser.py
@@ -9,10 +9,13 @@ executor = get_executor(job_name, timeout_hour=6, n_gpus=1, project='mdeq')
 with executor.batch():
     for use_bn in [True, False]:
         for use_res in [True, False]:
-            executor.submit(
-                train_mdeq_denoising,
-                n_val=20,
-                use_res=use_res,
-                use_bn=use_bn,
-                network_size='SMALL',
-            )
+            for use_new_residual in [True, False]:
+                executor.submit(
+                    train_mdeq_denoising,
+                    n_val=20,
+                    use_res=use_res,
+                    use_bn=use_bn,
+                    network_size='SMALL',
+                    use_new_residual=use_new_residual,
+                    grad_clipping=10.,
+                )
