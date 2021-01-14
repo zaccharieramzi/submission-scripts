0859471b3c7fe3fd096e571610d7bd2adcf1ff84
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-01-14 16:57:55 +0100

    added singlecoil sub scripts for ncpdnet

---
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/unet_singlecoil.py
@@ -0,0 +1,27 @@
+from fastmri_recon.evaluate.scripts.nc_eval import evaluate_unet as eval_fun
+from fastmri_recon.training_scripts.nc_train import train_unet_nc as train_fun
+
+from jean_zay.submitit.general_submissions import train_eval_grid, eval_grid
+
+params = {
+    'n_epochs': [100],
+    'af': [4],
+    'acq_type': ['radial', 'spiral'],
+    'loss': ['compound_mssim'],
+    'dcomp': [True],
+}
+
+eval_results = train_eval_grid(
+    'nc_unet',
+    train_fun,
+    eval_fun,
+    params,
+    n_gpus_train=1,
+    timeout_train=100,
+    n_gpus_eval=1,
+    n_samples_eval=30,
+    checkpoints_train=0,
+    resume_checkpoint=0,
+    resume_run_run_ids=None,
+)
+print(eval_results)
