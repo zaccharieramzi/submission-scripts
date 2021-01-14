a94592589218a498732b070b55be42b6d016aa9f
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-01-14 17:17:39 +0100

    added script for adjoint dcomp evaluation

---
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/adjoint_dcomp_singlecoil.py
@@ -0,0 +1,14 @@
+from fastmri_recon.evaluate.scripts.nc_eval import evaluate_dcomp
+
+from jean_zay.submitit.general_submissions import get_executor
+
+executor = get_executor('adjoint_dc', timeout_hour=20, n_gpus=1, project='fastmri')
+with executor.batch():
+    for acq_type in ['radial_stacks', 'spiral_stacks']:
+        executor.submit(
+            evaluate_dcomp,
+            acq_type=acq_type,
+            af=4,
+            n_samples=100,
+            dcomp=True,
+        )
