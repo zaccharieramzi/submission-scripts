ea0025fee6df16fdc371e0d1d088f3d0b84f7a89
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-01-14 18:10:23 +0100

    corrected default in dict

--- jean_zay/submitit/fastmri_reproducible_benchmark/xpdnet_singlecoil/testing_flmd_size.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/xpdnet_singlecoil/testing_flmd_size.py
@@ -31,7 +31,7 @@ with executor.batch():
                     res=res,
                     n_iter=n_iter,
                 )
-                results[(model_name, model_size)] = results.get((model_name, model_size), []).append(job)
+                results[(model_name, model_size)] = results.get((model_name, model_size), default=[]).append(job)
 
 
 for k, jobs in results.items():
