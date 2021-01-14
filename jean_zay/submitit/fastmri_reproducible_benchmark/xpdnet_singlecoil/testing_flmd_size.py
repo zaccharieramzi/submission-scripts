20cbad440b1c70b48e8b24ab4ffd7419adedc4d3
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-01-14 12:15:45 +0100

    WIP added testing size of flmd sub script

---
+++ jean_zay/submitit/fastmri_reproducible_benchmark/xpdnet_singlecoil/testing_flmd_size.py
@@ -0,0 +1,33 @@
+from fastmri_recon.models.subclassed_models.feature_level_multi_domain_learning.proposed_params import get_model_specs
+
+from jean_zay.submitit.general_submissions import get_executor
+from jean_zay.submitit.fastmri_reproducible_benchmark.mem_fitting_test import test_works_in_xpdnet_train
+
+n_iter_to_try_for_size = {
+    'small': list(range(20, 25)),
+    'medium': list(range(15, 20)),
+    'big': list(range(5, 15)),
+    'XL': list(range(1, 5))
+}
+n_primal = 5
+
+
+job_name = 'flmd_tryouts'
+executor = get_executor(job_name, timeout_hour=6, n_gpus=1, project='mdeq')
+
+results = {}
+
+with executor.batch():
+    for model_size_spec, n_iter_to_try in n_iter_to_try_for_size.items():
+        for n_iter in n_iter_to_try:
+            for model_name, model_size, model_fun, model_kwargs, n_inputs, n_scales, res in get_model_specs(n_primal):
+                if model_size != model_size_spec:
+                    continue
+                job = executor.submit(
+                    test_works_in_xpdnet_train,
+                    model_fun=model_fun,
+                    model_kwargs=model_kwargs,
+                    n_scales=n_scales,
+                    res=res,
+                    n_iter=n_iter,
+                )
