d6ae008a34b820b8dd96a338a9232edcad7811b5
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-01-20 16:50:34 +0100

    added multicoil sub scripts for non cartesian

---
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/ncpdnet_multicoil.py
@@ -0,0 +1,38 @@
+from fastmri_recon.evaluate.scripts.nc_eval import evaluate_ncpdnet as eval_fun
+from fastmri_recon.training_scripts.nc_train import train_ncpdnet as train_fun
+
+from jean_zay.submitit.general_submissions import train_eval_grid, eval_grid
+
+base_params = {
+    'n_epochs': [100],
+    'af': [4],
+    'acq_type': ['radial', 'spiral'],
+    'loss': ['compound_mssim'],
+    'multicoil': [True],
+    'refine_smaps': [True],
+}
+
+params = [
+  dict(dcomp=[True], normalize_image=[False], **base_params),
+  # dict(dcomp=[False], normalize_image=[True], **base_params),
+]
+
+# run_ids = [
+#     'ncpdnet_singlecoil___radial_compound_mssim_dcomp_1610872636',
+#     'ncpdnet_singlecoil___spiral_compound_mssim_dcomp_1610911070',
+# ]
+
+eval_results = train_eval_grid(
+    'nc_pdnet',
+    train_fun,
+    eval_fun,
+    params,
+    # run_ids=run_ids,
+    n_gpus_train=1,
+    timeout_train=100,
+    n_gpus_eval=1,
+    n_samples_eval=100,
+    # n_samples=100,
+    # n_gpus=1,
+)
+print(eval_results)
---
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/unet_multicoil.py
@@ -0,0 +1,33 @@
+from fastmri_recon.evaluate.scripts.nc_eval import evaluate_unet as eval_fun
+from fastmri_recon.training_scripts.nc_train import train_unet_nc as train_fun
+
+from jean_zay.submitit.general_submissions import train_eval_grid, eval_grid
+
+params = {
+    'n_epochs': [100],
+    'af': [4],
+    'acq_type': ['radial', 'spiral'],
+    'loss': ['compound_mssim'],
+    'dcomp': [True],
+    'multicoil': [True],
+}
+
+# run_ids = [
+    # 'unet_singlecoil___radial_compound_mssim_dcomp_1610911070',
+    # 'unet_singlecoil___spiral_compound_mssim_dcomp_1610911070',
+# ]
+
+eval_results = train_eval_grid(
+    'nc_unet',
+    # train_fun,
+    eval_fun,
+    params,
+    run_ids=run_ids,
+    n_gpus_train=1,
+    timeout_train=100,
+    n_gpus_eval=1,
+    n_samples_eval=100,
+    # n_samples=100,
+    # n_gpus=1,
+)
+print(eval_results)
