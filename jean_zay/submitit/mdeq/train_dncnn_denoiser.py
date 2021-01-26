1cf71cb8b8ef897724dcb3b10f6985fe63861c52
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-01-26 15:25:02 +0100

    updated ipdeq and dncnn sub script

--- jean_zay/submitit/mdeq/train_dncnn_denoiser.py
+++ jean_zay/submitit/mdeq/train_dncnn_denoiser.py
@@ -1,15 +1,29 @@
 from mdeq_lib.training_scripts.denoise_train import train_dncnn_denoising
+from mdeq_lib.evaluate.scripts.denoise_eval import eval_dncnn_denoising
 
-from jean_zay.submitit.general_submissions import get_executor
+from jean_zay.submitit.general_submissions import train_eval_grid
 
 
-job_name = 'ipdeq_denoise'
-executor = get_executor(job_name, timeout_hour=20, n_gpus=1, project='mdeq')
-
-executor.submit(
+job_name = 'dncnn_denoise'
+parameters = [
+    dict(
+        n_val=20,
+        network_size='SMALL',
+        grad_clipping=100.,
+        state_residual=True,
+    )
+]
+train_eval_grid(
+    job_name,
     train_dncnn_denoising,
-    n_val=20,
-    network_size='SMALL',
-    grad_clipping=100.,
-    state_residual=True,
+    eval_dncnn_denoising,
+    parameters,
+    to_grid=False,
+    timeout_train=20,
+    n_gpus_train=1,
+    timeout_eval=10,
+    n_gpus_eval=1,
+    project='mdeq',
+    params_to_ignore=['grad_clipping', 'n_val'],
+    val_noise_powers=[15, 25, 50],
 )
--- jean_zay/submitit/mdeq/train_ipdeq_denoiser.py
+++ jean_zay/submitit/mdeq/train_ipdeq_denoiser.py
@@ -1,17 +1,29 @@
 from mdeq_lib.training_scripts.denoise_train import train_ipdeq_denoising
+from mdeq_lib.evaluate.scripts.denoise_eval import eval_ipdeq_denoising
 
-from jean_zay.submitit.general_submissions import get_executor
+from jean_zay.submitit.general_submissions import train_eval_grid
 
 
 job_name = 'ipdeq_denoise'
-executor = get_executor(job_name, timeout_hour=20, n_gpus=1, project='mdeq')
+parameters = dict(
+    n_val=[20],
+    network_size=['SMALL'],
+    grad_clipping=[100.],
+    state_residual=[True],
+    with_dc=[True, False],
+)
 
-with executor.batch():
-    for with_dc in [True, False]:
-        executor.submit(
-            train_ipdeq_denoising,
-            n_val=20,
-            network_size='SMALL',
-            grad_clipping=100.,
-            with_dc=with_dc,
-        )
+train_eval_grid(
+    job_name,
+    train_ipdeq_denoising,
+    eval_ipdeq_denoising,
+    parameters,
+    to_grid=True,
+    timeout_train=20,
+    n_gpus_train=1,
+    timeout_eval=10,
+    n_gpus_eval=1,
+    project='mdeq',
+    params_to_ignore=['grad_clipping', 'n_val'],
+    val_noise_powers=[15, 25, 50],
+)
