4308fbd19db8afa3b10303de0958e27d53a51fd0
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-01-27 13:08:33 +0100

    switched dncnn to eval

--- jean_zay/submitit/mdeq/train_dncnn_denoiser.py
+++ jean_zay/submitit/mdeq/train_dncnn_denoiser.py
@@ -1,7 +1,7 @@
 from mdeq_lib.training_scripts.denoise_train import train_dncnn_denoising
 from mdeq_lib.evaluate.scripts.denoise_eval import eval_dncnn_denoising
 
-from jean_zay.submitit.general_submissions import train_eval_grid
+from jean_zay.submitit.general_submissions import train_eval_grid, eval_grid
 
 
 job_name = 'dncnn_denoise'
@@ -11,16 +11,19 @@ parameters = [
         grad_clipping=100.,
     )
 ]
-train_eval_grid(
+eval_grid(
     job_name,
-    train_dncnn_denoising,
+    # train_dncnn_denoising,
     eval_dncnn_denoising,
     parameters,
+    run_ids=[None],
     to_grid=False,
-    timeout_train=20,
-    n_gpus_train=1,
-    timeout_eval=10,
-    n_gpus_eval=1,
+    # timeout_train=20,
+    # n_gpus_train=1,
+    # timeout_eval=10,
+    # n_gpus_eval=1,
+    timeout=10,
+    n_gpus=1,
     project='mdeq',
     params_to_ignore=['grad_clipping', 'n_val'],
     val_noise_powers=[15, 25, 50],
