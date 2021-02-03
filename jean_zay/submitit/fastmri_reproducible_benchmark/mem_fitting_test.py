daf96560f5ffce11772eb4fad2ef41cc71245c2d
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-02-03 09:25:36 +0100

    corrrected mem fitting test

--- jean_zay/submitit/fastmri_reproducible_benchmark/mem_fitting_test.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/mem_fitting_test.py
@@ -6,24 +6,31 @@ from fastmri_recon.models.training.compile import default_model_compile
 
 n_primal = 5
 
-def test_works_in_xpdnet_train(model_fun, model_kwargs, n_scales, res, n_iter=10):
+def test_works_in_xpdnet_train(model_fun, model_kwargs, n_scales, res, n_iter=10, multicoil=False):
     run_params = {
         'n_primal': n_primal,
-        'multicoil': False,
+        'multicoil': multicoil,
         'n_scales': n_scales,
         'n_iter': n_iter,
-        'refine_smaps': False,
+        'refine_smaps': multicoil,
         'res': res,
     }
     model = XPDNet(model_fun, model_kwargs, **run_params)
     default_model_compile(model, lr=1e-3, loss='mae')
-    base_shape = 320
+    base_shape = 640
+    n_coils = 15
+    k_shape = (base_shape,)*2
+    if multicoil:
+        k_shape += (n_coils,)
+    inputs = [
+        tf.ones([1, *k_shape, 1], dtype=tf.complex64),
+        tf.ones([1, *k_shape], dtype=tf.complex64),
+    ]
+    if multicoil:
+        inputs += tf.ones([1, *k_shape], dtype=tf.complex64),
     try:
         model.fit(
-            x=[
-                tf.ones([1, base_shape, base_shape, 1], dtype=tf.complex64),
-                tf.ones([1, base_shape, base_shape], dtype=tf.complex64),
-            ],
+            x=,
             y=tf.ones([1, 320, 320, 1]),
             epochs=1,
         )
