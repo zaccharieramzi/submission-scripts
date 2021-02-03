c500eb22956f5131f6091ecc5207c4ca9d971bc2
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-02-03 17:16:40 +0100

    added adjoint dcomp eval for multicoil and 3d

---
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/adjoint_dcomp_3d.py
@@ -0,0 +1,14 @@
+from fastmri_recon.evaluate.scripts.nc_eval import evaluate_dcomp
+
+from jean_zay.submitit.general_submissions import get_executor
+
+executor = get_executor('adjoint_dc_3d', timeout_hour=20, n_gpus=1, project='fastmri4')
+with executor.batch():
+    for acq_type in ['radial', 'spiral']:
+        executor.submit(
+            evaluate_dcomp,
+            acq_type=acq_type,
+            af=4,
+            n_samples=100,
+            three_d=True,
+        )
