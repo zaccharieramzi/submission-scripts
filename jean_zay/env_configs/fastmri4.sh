bbc32ad702c9e6791debec5e7f3931e7d43abee7
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-02-05 13:52:39 +0100

    added tmp dir export in env configs

--- jean_zay/env_configs/fastmri4.sh
+++ jean_zay/env_configs/fastmri4.sh
@@ -6,4 +6,5 @@ export FASTMRI_DATA_DIR=$SCRATCH/
 export OASIS_DATA_DIR=$SCRATCH/OASIS_data
 export LOGS_DIR=$SCRATCH/
 export CHECKPOINTS_DIR=$SCRATCH/
+export TMP_DIR=$JOBSCRATCH/
 export SINGLECOIL_TRAIN_DIR=singlecoil_train/singlecoil_train
--- jean_zay/env_configs/mdeq.sh
+++ jean_zay/env_configs/mdeq.sh
@@ -4,6 +4,7 @@ module load python/3.7.5 cuda/10.1.2 cudnn/7.6.5.32-cuda-10.1 nccl/2.5.6-2-cuda
 conda activate mdeq
 
 export FASTMRI_DATA_DIR=$SCRATCH/
+export TMP_DIR=$JOBSCRATCH/
 export MDEQ_LOGS=$SCRATCH/logs
 export MDEQ_CHECKPOINTS=$SCRATCH/checkpoints
 export MDEQ_DATA=$SCRATCH/
