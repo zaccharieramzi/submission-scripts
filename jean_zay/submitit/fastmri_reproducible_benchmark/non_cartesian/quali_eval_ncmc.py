dd122ed8b91bfcf917f9027200a54b643a77ef78
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-02-09 12:15:34 +0100

    added quali val script for ncmc

---
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/quali_eval_ncmc.py
@@ -0,0 +1,37 @@
+from fastmri_recon.data.scripts.qualitative_validation import nc_multinet_qualitative_validation
+
+from jean_zay.submitit.general_submissions import get_executor
+
+
+model_2_run_ids = {
+    'pdnet': {
+        'radial': 'ncpdnet_sense___rfs_radial_compound_mssim_dcomp_1611913984',
+        'spiral': 'ncpdnet_sense___rfs_spiral_compound_mssim_dcomp_1611913984',
+    },
+    'unet': {
+        'radial': 'unet_mc___radial_compound_mssim_dcomp_1611915508',
+        'spiral': 'unet_mc___spiral_compound_mssim_dcomp_1611915508',
+    },
+    'adj-dcomp': {
+        'radial': None,
+        'spiral': None,
+    },
+}
+
+
+executor = get_executor('ncmc_quali', timeout_hour=20, n_gpus=1, project='fastmri4')
+with executor.batch():
+    for model, run_ids in model_2_run_ids.items():
+        for acq_type in ['radial', 'spiral']:
+            executor.submit(
+                nc_multinet_qualitative_validation,
+                acq_type=acq_type,
+                af=4,
+                model=model,
+                run_id=run_ids[acq_type],
+                refine_smaps=True,
+                dcomp=True,
+                normalize_image=False,
+                contrast='CORPD_FBK',
+                n_epochs=100,
+            )
