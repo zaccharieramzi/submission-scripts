2b0edbca3fc2110d7caa385cd0c77c3a72cf16be
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-02-09 16:09:57 +0100

    corrected timeout for quali scripts

--- jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/quali_eval_3d.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/quali_eval_3d.py
@@ -19,7 +19,7 @@ model_2_run_ids = {
 }
 
 
-executor = get_executor('3dnc_quali', timeout_hour=20, n_gpus=1, project='fastmri4')
+executor = get_executor('3dnc_quali', timeout_hour=2, n_gpus=1, project='fastmri4')
 with executor.batch():
     for model, run_ids in model_2_run_ids.items():
         for acq_type in ['radial_stacks', 'spiral_stacks']:
