79e668c9903f8db57a33f28d7d2820ff1cd6a5a0
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-02-16 16:37:14 +0100

    corrected tf records ncmc gen for brain to only val

--- jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/ncmc_tf_records_gen.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/ncmc_tf_records_gen.py
@@ -5,6 +5,7 @@ from jean_zay.submitit.general_submissions import get_executor
 
 executor = get_executor('ncmc_tfrecords', timeout_hour=20, n_gpus=1, project='fastmri')
 with executor.batch():
-    for mode in ['train', 'val']:
+    # for mode in ['train', 'val']:
+    for mode in ['val']:
         for acq_type in ['radial', 'spiral']:
             executor.submit(generate_multicoil_nc_tf_records, acq_type=acq_type, af=4, mode=mode, brain=True)
