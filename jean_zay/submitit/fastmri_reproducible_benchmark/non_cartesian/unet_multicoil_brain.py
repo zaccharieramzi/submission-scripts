31c055f3a32404ed7970f13d75917d871c012585
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-02-16 16:40:43 +0100

    added evaluation scripts for brain data

---
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/unet_multicoil_brain.py
@@ -0,0 +1,29 @@
+from fastmri_recon.evaluate.scripts.nc_eval import evaluate_unet as eval_fun
+
+from jean_zay.submitit.general_submissions import train_eval_grid, eval_grid
+
+params = {
+    'n_epochs': [100],
+    'af': [4],
+    'acq_type': ['radial', 'spiral'],
+    'loss': ['compound_mssim'],
+    'dcomp': [True],
+    'multicoil': [True],
+    'brain': [True],
+}
+
+run_ids = [
+    'unet_mc___radial_compound_mssim_dcomp_1611915508',
+    'unet_mc___spiral_compound_mssim_dcomp_1611915508',
+]
+
+eval_results = eval_grid(
+    'nc_unet_mc_brain',
+    eval_fun,
+    params,
+    run_ids=run_ids,
+    n_samples=250,
+    n_gpus=3,
+    timeout=20,
+)
+print(eval_results)
