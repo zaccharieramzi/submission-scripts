f7d090a07a50f92041a9456311fd81bff71d049d
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-02-19 16:56:31 +0100

    added sub script with different restarting strategy for ipdeq

---
+++ jean_zay/submitit/mdeq/train_ipdeq_denoiser_restarts.py
@@ -0,0 +1,32 @@
+from mdeq_lib.training_scripts.denoise_train import train_ipdeq_denoising
+from mdeq_lib.evaluate.scripts.denoise_eval import eval_ipdeq_denoising
+
+from jean_zay.submitit.general_submissions import train_eval_grid
+
+
+job_name = 'ipdeq_denoise'
+parameters = dict(
+    n_val=[20],
+    grad_clipping=[100.],
+    state_residual=[True],
+    with_dc=[True, False],
+    loss=['mae'],
+    n_epochs=[2000],
+    fixed_lr=[True],
+    restart_optimizer_method=['refresh', 'refresh-all', 'low_lr'],
+)
+
+train_eval_grid(
+    job_name,
+    train_ipdeq_denoising,
+    eval_ipdeq_denoising,
+    parameters,
+    to_grid=True,
+    timeout_train=20,
+    n_gpus_train=1,
+    timeout_eval=10,
+    n_gpus_eval=1,
+    project='mdeq',
+    params_to_ignore=['grad_clipping', 'n_val'],
+    val_noise_powers=[15, 25, 50],
+)
