7160e61f6ba029de17bcba3816228e01abf4ea23
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-03-04 18:26:24 +0100

    added noise conditionned sub scripts for dncnn and ipdeq

---
+++ jean_zay/submitit/mdeq/train_dncnn_auto_noise_cond_denoiser.py
@@ -0,0 +1,34 @@
+from mdeq_lib.training_scripts.denoise_train import train_dncnn_denoising
+from mdeq_lib.evaluate.scripts.denoise_eval import eval_dncnn_denoising
+
+from jean_zay.submitit.general_submissions import train_eval_grid, eval_grid
+
+
+job_name = 'dncnn_denoise'
+parameters = dict(
+    n_val=[20],
+    grad_clipping=[100],
+    fixed_lr=[True],
+    loss=['mae'],
+    n_epochs=[5000],
+    noise_conditionning=[True, False],
+    noise_residual_weighting=[True, False],
+)
+train_eval_grid(
+    job_name,
+    train_dncnn_denoising,
+    eval_dncnn_denoising,
+    parameters,
+    # run_ids=[None],
+    to_grid=True,
+    timeout_train=60,
+    n_gpus_train=1,
+    timeout_eval=2,
+    n_gpus_eval=1,
+    # timeout=10,
+    # n_gpus=1,
+    project='mdeq',
+    params_to_ignore=['grad_clipping', 'n_val'],
+    val_noise_powers=[15, 25, 50],
+    no_force_32=True,
+)
