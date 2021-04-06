dfb8c2910dbe4162eae4ff03c06638afe99ba94b
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-06 23:08:13 +0200

    reduced cifar tiny time evn more

--- jean_zay/submitit/shine/cifar_tiny.py
+++ jean_zay/submitit/shine/cifar_tiny.py
@@ -5,7 +5,7 @@ from jean_zay.submitit.general_submissions import get_executor
 
 job_name = 'shine_classifier_cifar_small'
 n_gpus = 4
-executor = get_executor(job_name, timeout_hour=20, n_gpus=n_gpus, project='shine')
+executor = get_executor(job_name, timeout_hour=2, n_gpus=n_gpus, project='shine')
 
 with executor.batch():
     for shine in [True, False]:
