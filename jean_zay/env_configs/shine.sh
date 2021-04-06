a645ee054e141472f3791c182d16d9795e65e613
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-06 17:33:18 +0200

    added cifar dir to shine config

--- jean_zay/env_configs/shine.sh
+++ jean_zay/env_configs/shine.sh
@@ -4,6 +4,7 @@ module load pytorch-gpu/py3/1.6.0
 
 export FASTMRI_DATA_DIR=$SCRATCH/
 export IMAGENET_DIR=$SCRATCH/imagenet/
+export CIFAR_DIR=$SCRATCH/cifar/
 export TMP_DIR=$JOBSCRATCH/
 export SHINE_LOGS=$SCRATCH/logs
 export SHINE_CHECKPOINTS=$SCRATCH/checkpoints
