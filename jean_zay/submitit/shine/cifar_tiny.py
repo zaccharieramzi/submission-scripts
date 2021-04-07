a8149b119c3647af9142d4a85cc1a3985f4800d0
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-07 10:39:37 +0200

    corrected statistical test import

--- jean_zay/submitit/shine/cifar_tiny.py
+++ jean_zay/submitit/shine/cifar_tiny.py
@@ -1,6 +1,6 @@
 from mdeq_lib.evaluate.cls_valid import evaluate_classifier
 from mdeq_lib.training.cls_train import train_classifier
-from scipy.tests import ttest_ind
+from scipy.stats import ttest_ind
 
 from jean_zay.submitit.general_submissions import train_eval_grid
 
