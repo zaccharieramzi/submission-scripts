8f2958e032d51f4700322a28eb17f50d2bc4fe7e
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-07 10:16:23 +0200

    added fpn sub script for debugging

---
+++ jean_zay/submitit/shine/cifar_tiny_fpn.py
@@ -0,0 +1,18 @@
+from mdeq_lib.training.cls_train import train_classifier
+
+from jean_zay.submitit.general_submissions import get_executor
+
+
+job_name = 'shine_classifier_cifar_small_fpn'
+n_gpus = 4
+executor = get_executor(job_name, timeout_hour=1, n_gpus=n_gpus, project='shine')
+
+executor.submit(
+    train_classifier,
+    model_size='TINY',
+    dataset='cifar',
+    n_gpus=n_gpus,
+    shine=False,
+    fpn=True,
+    n_epochs=25,
+)
