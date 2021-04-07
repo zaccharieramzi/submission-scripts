a5c7f0030428d3315d01b2838fa457a05b9685a8
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-07 14:21:03 +0200

    corrected sub script for fpn

--- jean_zay/submitit/shine/cifar_tiny_fpn.py
+++ jean_zay/submitit/shine/cifar_tiny_fpn.py
@@ -5,7 +5,14 @@ from jean_zay.submitit.general_submissions import get_executor
 
 job_name = 'shine_classifier_cifar_small_fpn'
 n_gpus = 4
-executor = get_executor(job_name, timeout_hour=1, n_gpus=n_gpus, project='shine')
+executor = get_executor(
+    job_name, 
+    timeout_hour=1,
+    n_gpus=n_gpus,
+    project='shine'
+    torch=True,
+    no_force32=True,
+)
 
 executor.submit(
     train_classifier,
@@ -15,6 +22,5 @@ executor.submit(
     shine=False,
     fpn=True,
     n_epochs=25,
-    torch=True,
-    no_force32=True,
+
 )
