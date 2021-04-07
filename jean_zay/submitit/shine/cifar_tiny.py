f36a205caac5445f9401099bb4c10cf512ddff85
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-07 10:35:11 +0200

    created submission script for multiple runs in cifar tiny with statistical test at the end

--- jean_zay/submitit/shine/cifar_tiny.py
+++ jean_zay/submitit/shine/cifar_tiny.py
@@ -1,19 +1,52 @@
+from mdeq_lib.evaluate.cls_valid import evaluate_classifier
 from mdeq_lib.training.cls_train import train_classifier
+from scipy.tests import ttest_ind
 
-from jean_zay.submitit.general_submissions import get_executor
+from jean_zay.submitit.general_submissions import train_eval_grid
 
 
 job_name = 'shine_classifier_cifar_small'
+n_runs = 5
 n_gpus = 4
-executor = get_executor(job_name, timeout_hour=2, n_gpus=n_gpus, project='shine')
+base_params = dict(
+    model_size='TINY',
+    dataset='cifar',
+    n_gpus=n_gpus,
+    n_epochs=50,
+)
+parameters = [
+    base_params,
+    dict(shine=True, **base_params),
+    dict(fpn=True, **base_params),
+]
 
-with executor.batch():
-    for shine in [True, False]:
-        executor.submit(
-            train_classifier,
-            model_size='TINY',
-            dataset='cifar',
-            n_gpus=n_gpus,
-            shine=shine,
-            n_epochs=50,
-        )
+
+perf_orig = []
+perf_shine = []
+perf_fpn = []
+for i_run in range(n_runs):
+    res_orig, res_shine, res_fpn = train_eval_grid(
+        job_name,
+        train_classifier,
+        evaluate_classifier,
+        parameters,
+        to_grid=False,
+        timeout_train=2,
+        n_gpus_train=n_gpus,
+        timeout_eval=1,
+        n_gpus_eval=n_gpus,
+        project='shine',
+        params_to_ignore=['n_epochs'],
+        torch=True,
+        no_force_32=True,
+    )
+    perf_orig.append(res_orig)
+    perf_shine.append(res_shine)
+    perf_fpn.append(res_fpn)
+
+print('Perf orig', perf_orig)
+print('Perf shine', perf_shine)
+print('Perf fpn', perf_fpn)
+
+print('Stats test orig vs shine', ttest_ind(perf_orig, perf_shine))
+print('Stats test orig vs fpn', ttest_ind(perf_orig, perf_fpn))
