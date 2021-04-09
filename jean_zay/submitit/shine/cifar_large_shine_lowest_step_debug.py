bd0988a1630863ee2b7191f57a33510c439d852c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-09 08:38:47 +0200

    added lowest step debug job

---
+++ jean_zay/submitit/shine/cifar_large_shine_lowest_step_debug.py
@@ -0,0 +1,35 @@
+from mdeq_lib.evaluate.cls_valid import evaluate_classifier
+from mdeq_lib.training.cls_train import train_classifier
+
+from jean_zay.submitit.general_submissions import train_eval_grid
+
+
+job_name = 'shine_classifier_cifar_large_debug_shine'
+n_gpus = 4
+base_params = dict(
+    model_size='LARGE',
+    dataset='cifar',
+    n_gpus=n_gpus,
+    n_epochs=220,
+    save_at=50,
+    restart_fro=50
+)
+
+parameters = [
+    dict(seed=0, shine=True, **base_params),
+]
+
+train_eval_grid(
+    job_name,
+    train_classifier,
+    evaluate_classifier,
+    parameters,
+    to_grid=False,
+    timeout_train=20,
+    n_gpus_train=n_gpus,
+    timeout_eval=2,
+    n_gpus_eval=n_gpus,
+    project='shine',
+    params_to_ignore=['n_epochs', 'save_at'],
+    torch=True,
+)
