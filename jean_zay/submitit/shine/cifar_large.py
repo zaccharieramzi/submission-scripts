8d5caea9a144b1f063d283ff2d2ad0cab4f5d60f
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-09 10:01:53 +0200

    added clear parsing of results for cifar large

--- jean_zay/submitit/shine/cifar_large.py
+++ jean_zay/submitit/shine/cifar_large.py
@@ -1,5 +1,7 @@
 from mdeq_lib.evaluate.cls_valid import evaluate_classifier
 from mdeq_lib.training.cls_train import train_classifier
+import numpy as np
+from scipy.stats import ttest_ind
 
 from jean_zay.submitit.general_submissions import train_eval_grid
 
@@ -21,7 +23,7 @@ for i_run in range(n_runs):
         dict(seed=i_run, fpn=True, **base_params),
     ]
 
-train_eval_grid(
+res_all = train_eval_grid(
     job_name,
     train_classifier,
     evaluate_classifier,
@@ -35,3 +37,21 @@ train_eval_grid(
     params_to_ignore=['n_epochs', 'save_at'],
     torch=True,
 )
+
+
+perf_orig = [res for (res, params) in zip(res_all, parameters) if not params['shine'] and not params['fpn']]
+perf_shine = [res for (res, params) in zip(res_all, parameters) if params['shine']]
+perf_fpn = [res for (res, params) in zip(res_all, parameters) if params['fpn']]
+
+
+print('Perf orig', perf_orig)
+print('Perf shine', perf_shine)
+print('Perf fpn', perf_fpn)
+
+print('Stats test orig vs shine', ttest_ind(perf_orig, perf_shine))
+print('Stats test orig vs fpn', ttest_ind(perf_orig, perf_fpn))
+
+print('Descriptive stats')
+print('Perf orig', np.mean(perf_orig), np.std(perf_orig))
+print('Perf shine', np.mean(perf_shine), np.std(perf_shine))
+print('Perf fpn', np.mean(perf_fpn), np.std(perf_fpn))
