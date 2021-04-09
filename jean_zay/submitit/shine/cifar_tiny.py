b3a10610033ab3d9e6059391d4b1be1057fe11bf
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-09 08:24:12 +0200

    added descriptive stats at the end of cifar tiny

--- jean_zay/submitit/shine/cifar_tiny.py
+++ jean_zay/submitit/shine/cifar_tiny.py
@@ -1,5 +1,6 @@
 from mdeq_lib.evaluate.cls_valid import evaluate_classifier
 from mdeq_lib.training.cls_train import train_classifier
+import numpy as np
 from scipy.stats import ttest_ind
 
 from jean_zay.submitit.general_submissions import train_eval_grid
@@ -51,3 +52,8 @@ print('Perf fpn', perf_fpn)
 
 print('Stats test orig vs shine', ttest_ind(perf_orig, perf_shine))
 print('Stats test orig vs fpn', ttest_ind(perf_orig, perf_fpn))
+
+print('Descriptive stats')
+print('Perf orig', np.mean(perf_orig), np.std(perf_orig))
+print('Perf shine', np.mean(perf_shine), np.std(perf_shine))
+print('Perf fpn', np.mean(perf_fpn), np.std(perf_fpn))
