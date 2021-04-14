042c5cac92cd2580ff94006091bfcedbe8e4a116
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-14 17:10:14 +0200

    added contractivity sub script

---
+++ jean_zay/submitit/shine/cifar_tiny_contract.py
@@ -0,0 +1,34 @@
+from mdeq_lib.evaluate.cls_valid import evaluate_classifier
+
+from jean_zay.submitit.general_submissions import eval_grid
+
+
+job_name = 'shine_classifier_cifar_small_contract'
+n_gpus = 4
+base_params = dict(
+    model_size='TINY',
+    dataset='cifar',
+    n_gpus=n_gpus,
+    check_contract=True,
+    n_iter=20,
+    seed=0,
+)
+parameters = []
+parameters += [
+    base_params,
+    dict(shine=True, **base_params),
+    dict(fpn=True, **base_params),
+]
+
+res_all = eval_grid(
+    job_name,
+    evaluate_classifier,
+    parameters,
+    to_grid=False,
+    timeout=1,
+    n_gpus=n_gpus,
+    project='shine',
+    params_to_ignore=['n_epochs'],
+    torch=True,
+    no_force_32=True,
+)
