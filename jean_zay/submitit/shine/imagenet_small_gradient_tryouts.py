356e6c1e397d4c2f0a52ea3f93a7a9c58322b9f1
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-20 16:20:59 +0200

    added optimizer tryouts sub script

---
+++ jean_zay/submitit/shine/imagenet_small_gradient_tryouts.py
@@ -0,0 +1,33 @@
+from mdeq_lib.evaluate.cls_valid import evaluate_classifier
+from mdeq_lib.training.cls_train import train_classifier
+
+from jean_zay.submitit.general_submissions import train_eval_grid
+
+
+job_name = 'shine_classifier_imagenet_small'
+n_gpus = 4
+base_params = dict(
+    dataset='imagenet',
+    n_gpus=n_gpus,
+    n_epochs=100,
+    seed=0,
+)
+parameters = [
+    dict(shine=True, model_size='SMALL_clip', restart_from=48, **base_params),
+    dict(shine=True, model_size='SMALL_adam', **base_params),
+]
+
+train_eval_grid(
+    job_name,
+    train_classifier,
+    evaluate_classifier,
+    parameters,
+    to_grid=False,
+    timeout_train=100,
+    n_gpus_train=n_gpus,
+    timeout_eval=20,
+    n_gpus_eval=n_gpus,
+    project='shine',
+    params_to_ignore=['n_epochs', 'restart_from'],
+    torch=True,
+)
