680c6297c6493487e981020c6bc5367c7976c6ee
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-23 10:21:19 +0200

    switched imagenet tiny in pretraining mode with only the base params and saving at 48

--- jean_zay/submitit/shine/imagenet_tiny.py
+++ jean_zay/submitit/shine/imagenet_tiny.py
@@ -12,11 +12,12 @@ base_params = dict(
     n_gpus=n_gpus,
     n_epochs=100,
     seed=0,
+    save_at=48,
 )
 parameters = [
     base_params,
-    dict(shine=True, **base_params),
-    dict(fpn=True, **base_params),
+    # dict(shine=True, **base_params),
+    # dict(fpn=True, **base_params),
 ]
 
 train_eval_grid(
@@ -25,11 +26,11 @@ train_eval_grid(
     evaluate_classifier,
     parameters,
     to_grid=False,
-    timeout_train=100,
+    timeout_train=40,
     n_gpus_train=n_gpus,
     timeout_eval=20,
     n_gpus_eval=n_gpus,
     project='shine',
-    params_to_ignore=['n_epochs', 'restart_from'],
+    params_to_ignore=['n_epochs', 'restart_from', 'save_at'],
     torch=True,
 )
