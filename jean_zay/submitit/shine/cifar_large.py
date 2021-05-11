4a992656d11d4656719d5753bc336f08cbd81dd9
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-05-11 10:40:51 +0200

    made cifar large restart from 50 to save time

--- jean_zay/submitit/shine/cifar_large.py
+++ jean_zay/submitit/shine/cifar_large.py
@@ -15,11 +15,12 @@ base_params = dict(
     n_gpus=n_gpus,
     n_epochs=220,
     save_at=50,
+    restart_from=50,
 )
 parameters = []
 for i_run in range(n_runs):
     parameters += [
-        dict(seed=i_run, **base_params),
+        # dict(seed=i_run, **base_params),
         dict(seed=i_run, shine=True, **base_params),
         dict(seed=i_run, fpn=True, **base_params),
     ]
@@ -40,19 +41,19 @@ res_all = train_eval_grid(
 )
 
 
-perf_orig = [res for (res, params) in zip(res_all, parameters) if not params.get('shine', False) and not params.get('fpn', False)]
+# perf_orig = [res for (res, params) in zip(res_all, parameters) if not params.get('shine', False) and not params.get('fpn', False)]
 perf_shine = [res for (res, params) in zip(res_all, parameters) if params.get('shine', False)]
 perf_fpn = [res for (res, params) in zip(res_all, parameters) if params.get('fpn', False)]
 
 
-print('Perf orig', perf_orig)
-print('Perf shine', perf_shine)
-print('Perf fpn', perf_fpn)
-
-print('Stats test orig vs shine', ttest_ind(perf_orig, perf_shine))
-print('Stats test orig vs fpn', ttest_ind(perf_orig, perf_fpn))
+# print('Perf orig', perf_orig)
+# print('Perf shine', perf_shine)
+# print('Perf fpn', perf_fpn)
+#
+# print('Stats test orig vs shine', ttest_ind(perf_orig, perf_shine))
+# print('Stats test orig vs fpn', ttest_ind(perf_orig, perf_fpn))
 
 print('Descriptive stats')
-print('Perf orig', np.mean(perf_orig), np.std(perf_orig))
+# print('Perf orig', np.mean(perf_orig), np.std(perf_orig))
 print('Perf shine', np.mean(perf_shine), np.std(perf_shine))
 print('Perf fpn', np.mean(perf_fpn), np.std(perf_fpn))
