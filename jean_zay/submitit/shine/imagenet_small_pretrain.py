ef5de4071972c5b7b48b805cd74a8bcde2738547
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-05-12 13:02:13 +0200

    refreshed imagenet pretraining

--- jean_zay/submitit/shine/imagenet_small_pretrain.py
+++ jean_zay/submitit/shine/imagenet_small_pretrain.py
@@ -1,7 +1,6 @@
 from mdeq_lib.evaluate.cls_valid import evaluate_classifier
 from mdeq_lib.training.cls_train import train_classifier
-import numpy as np
-from scipy.stats import ttest_ind
+
 
 from jean_zay.submitit.general_submissions import train_eval_grid
 
@@ -15,7 +14,7 @@ base_params = dict(
     n_gpus=n_gpus,
     n_epochs=100,
     save_at=50,
-    restart_from=48,
+    # restart_from=48,
 )
 parameters = []
 for i_run in range(n_runs):
@@ -31,7 +30,7 @@ res_all = train_eval_grid(
     evaluate_classifier,
     parameters,
     to_grid=False,
-    timeout_train=10,
+    timeout_train=100,
     n_gpus_train=n_gpus,
     timeout_eval=2,
     n_gpus_eval=n_gpus,
@@ -40,20 +39,4 @@ res_all = train_eval_grid(
     torch=True,
 )
 
-
-perf_orig = [res for (res, params) in zip(res_all, parameters) if not params.get('shine', False) and not params.get('fpn', False)]
-perf_shine = [res for (res, params) in zip(res_all, parameters) if params.get('shine', False)]
-perf_fpn = [res for (res, params) in zip(res_all, parameters) if params.get('fpn', False)]
-
-
-print('Perf orig', perf_orig)
-print('Perf shine', perf_shine)
-print('Perf fpn', perf_fpn)
-
-print('Stats test orig vs shine', ttest_ind(perf_orig, perf_shine))
-print('Stats test orig vs fpn', ttest_ind(perf_orig, perf_fpn))
-
-print('Descriptive stats')
-print('Perf orig', np.mean(perf_orig), np.std(perf_orig))
-print('Perf shine', np.mean(perf_shine), np.std(perf_shine))
-print('Perf fpn', np.mean(perf_fpn), np.std(perf_fpn))
+print(res_all)
