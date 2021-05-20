d8fefc56693b24c8731f47bb408fb0b3492a824f
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-05-20 12:21:04 +0200

    in debug mode adj broyden using only 2h

--- jean_zay/submitit/shine/cifar_tiny_adjbroyden.py
+++ jean_zay/submitit/shine/cifar_tiny_adjbroyden.py
@@ -34,7 +34,7 @@ res_all = train_eval_grid(
     evaluate_classifier,
     parameters,
     to_grid=False,
-    timeout_train=20,
+    timeout_train=2,
     n_gpus_train=n_gpus,
     timeout_eval=1,
     n_gpus_eval=n_gpus,
