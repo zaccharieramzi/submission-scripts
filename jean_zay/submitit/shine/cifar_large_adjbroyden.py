05163fbc08ed5eaa5b533ef32c089a665f12bab4
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-05-21 18:28:35 +0200

    got the cifar large script ready for adj broyden

---
+++ jean_zay/submitit/shine/cifar_large_adjbroyden.py
@@ -0,0 +1,49 @@
+from mdeq_lib.evaluate.cls_valid import evaluate_classifier
+from mdeq_lib.training.cls_train import train_classifier
+import numpy as np
+from scipy.stats import ttest_ind
+
+from jean_zay.submitit.general_submissions import train_eval_grid
+
+
+job_name = 'shine_classifier_cifar_large_adjbroyden'
+n_gpus = 4
+n_runs = 5
+base_params = dict(
+    model_size='LARGE',
+    dataset='cifar',
+    n_gpus=n_gpus,
+    n_epochs=220,
+    save_at=50,
+    restart_from=50,
+    adjoint_broyden=True,
+)
+parameters = []
+for i_run in range(n_runs):
+    parameters += [
+        dict(seed=i_run, **base_params),
+        dict(seed=i_run, opa=True, **base_params),
+    ]
+
+res_all = train_eval_grid(
+    job_name,
+    train_classifier,
+    evaluate_classifier,
+    parameters,
+    to_grid=False,
+    timeout_train=20,
+    n_gpus_train=n_gpus,
+    timeout_eval=2,
+    n_gpus_eval=n_gpus,
+    project='shine',
+    params_to_ignore=['n_epochs', 'save_at', 'restart_from'],
+    torch=True,
+)
+
+
+perf_shine_adj_br = [res for (res, params) in zip(res_all, parameters) if not params.get('opa', False)]
+perf_shine_opa = [res for (res, params) in zip(res_all, parameters) if params.get('opa', False)]
+
+print('Descriptive stats')
+print('Perf shine', np.mean(perf_shine_adj_br), np.std(perf_shine_adj_br))
+print('Perf shine', np.mean(perf_shine_opa), np.std(perf_shine_opa))
