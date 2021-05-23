74dd749103d05e0964bc712a47d55cded94fcbfc
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-05-23 16:04:47 +0200

    added sub script for adj broyden correl

---
+++ jean_zay/submitit/shine/debug/adj_broyden_controlled_correl.py
@@ -0,0 +1,22 @@
+from mdeq_lib.tests.modules.adj_broyden_correl import save_results
+
+from jean_zay.submitit.general_submissions import get_executor
+
+
+job_name = 'adj_broyden_correl'
+n_gpus = 1
+base_params = dict(
+    n_runs=100,
+    random_prescribed=False,
+)
+parameters = [
+    dict(dataset='imagenet', model_size='SMALL', **base_params),
+    dict(dataset='cifar', model_size='LARGE', **base_params),
+]
+
+executor = get_executor(job_name, timeout_hour=2, n_gpus=n_gpus, project='shine')
+jobs = []
+with executor.batch():
+    for param in parameters:
+        job = executor.submit(save_results, **param)
+        jobs.append(job)
