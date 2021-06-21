1972e30cad374c559533922f346aa17729b6019b
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-06-21 16:10:41 +0200

    corrected mem needs for tfrecords generation sub scripts, also corrected project, and added af8 for mc

--- jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/ncmc_tf_records_gen.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/ncmc_tf_records_gen.py
@@ -3,9 +3,10 @@ from fastmri_recon.data.scripts.multicoil_nc_tf_records_generation import genera
 from jean_zay.submitit.general_submissions import get_executor
 
 
-executor = get_executor('ncmc_tfrecords', timeout_hour=20, n_gpus=1, project='fastmri4')
+executor = get_executor('ncmc_tfrecords', timeout_hour=20, n_gpus=1, project='fastmri4', no_force_32=True)
 with executor.batch():
     # for mode in ['train', 'val']:
     for mode in ['val']:
         for acq_type in ['radial', 'spiral']:
-            executor.submit(generate_multicoil_nc_tf_records, acq_type=acq_type, af=4, mode=mode, brain=True)
+            for af in [4, 8]:
+                executor.submit(generate_multicoil_nc_tf_records, acq_type=acq_type, af=af, mode=mode, brain=True)
--- jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/oasis_tf_records_gen.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/oasis_tf_records_gen.py
@@ -3,7 +3,7 @@ from fastmri_recon.data.scripts.oasis_tf_records_generation import generate_oasi
 from jean_zay.submitit.general_submissions import get_executor
 
 
-executor = get_executor('oasis_tfrecords', timeout_hour=20, n_gpus=1, project='fastmri')
+executor = get_executor('oasis_tfrecords', timeout_hour=20, n_gpus=1, project='fastmri4', no_force_32=True)
 with executor.batch():
     for mode in ['train', 'val']:
         for acq_type in ['radial_stacks', 'spiral_stacks']:
