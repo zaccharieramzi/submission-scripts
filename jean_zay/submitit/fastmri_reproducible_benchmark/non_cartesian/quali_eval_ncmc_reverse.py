a2d87bca632c6f1225c8b52ef459168d077003f4
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-06-25 15:50:34 +0200

    corrected contrast used for quali fig in mc sub script

--- jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/quali_eval_ncmc_reverse.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/quali_eval_ncmc_reverse.py
@@ -29,7 +29,7 @@ with executor.batch():
                 refine_smaps=True,
                 dcomp=True,
                 normalize_image=False,
-                contrast='CORPD_FBK',
+                contrast='CORPDFS_FBK',
                 n_epochs=100,
                 # zoom=[(200, 300), (100, 200)],
                 draw_zoom=[(200, 300), (100, 200)],
