575cf68deb641093eea0d953469d432601c3629c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-06-29 11:06:47 +0200

    added a sub script to debug dip

---
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/debug_dip.py
@@ -0,0 +1,18 @@
+from fastmri_recon.evaluate.scripts.debug_nc_dip_eval import debug_dip_nc
+
+from jean_zay.submitit.general_submissions import get_executor
+
+
+executor = get_executor('debug_dip', timeout_hour=2, n_gpus=1, project='fastmri4')
+job = executor.submit(
+    debug_dip_nc,
+    acq_type='radial',
+    af=4,
+    contrast='CORPD_FBK',
+    model_kwargs={'bn': True},
+    n_iter=10,
+)
+
+save_path, history = job.result()
+print(save_path)
+import ipdb; ipdb.set_trace()
