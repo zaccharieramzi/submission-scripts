beec373af7b168d5ca429e7e464d0cb57c652539
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-06-29 12:53:33 +0200

    switched to pure radial for oasis tf records

--- jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/oasis_tf_records_gen.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/oasis_tf_records_gen.py
@@ -6,5 +6,6 @@ from jean_zay.submitit.general_submissions import get_executor
 executor = get_executor('oasis_tfrecords', timeout_hour=20, n_gpus=1, project='fastmri4', no_force_32=True)
 with executor.batch():
     for mode in ['train', 'val']:
-        for acq_type in ['radial_stacks', 'spiral_stacks']:
+        # for acq_type in ['radial_stacks', 'spiral_stacks']:
+        for acq_type in ['radial']:
             executor.submit(generate_oasis_tf_records, acq_type=acq_type, af=4, mode=mode)
