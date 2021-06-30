88801055f291ea4c7a851c1a1d0d86d809f1c4d6
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-06-30 15:22:19 +0200

    made dip single coil eval in final state

--- jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/dip_singlecoil.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/dip_singlecoil.py
@@ -3,11 +3,9 @@ from fastmri_recon.evaluate.scripts.nc_dip_eval import evaluate_dip_nc as eval_f
 from jean_zay.submitit.general_submissions import eval_grid
 
 base_params = {
-    'af': [4],
-    # 'acq_type': ['radial', 'spiral'],
-    'acq_type': ['radial'],
-    # 'contrast': ['CORPD_FBK', 'CORPDFS_FBK'],
-    'contrast': ['CORPD_FBK',],
+    'af': [4, 8],
+    'acq_type': ['radial', 'spiral'],
+    'contrast': ['CORPD_FBK', 'CORPDFS_FBK'],
     'model_kwargs': [{'bn': True, 'n_up': 5}],
 }
 
@@ -17,9 +15,9 @@ eval_results = eval_grid(
     eval_fun,
     base_params,
     run_ids=None,
-    n_samples=10,
+    n_samples=100,
     n_gpus=1,
-    timeout=20,
+    timeout=100,
     project='fastmri4',
 )
 print(eval_results)
