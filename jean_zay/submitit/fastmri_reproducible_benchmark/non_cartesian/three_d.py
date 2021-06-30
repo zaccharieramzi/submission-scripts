222c6142f39b60090999fcb2bf4bec3c57077f12
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-06-30 09:41:34 +0200

    switched to radial for 3d training

--- jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/three_d.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/three_d.py
@@ -11,7 +11,7 @@ base_params = {
     'n_filters': [16],
     'n_iter': [6],
     'n_primal': [2],
-    'acq_type': ['radial_stacks', 'spiral_stacks'],
+    'acq_type': ['radial'],
     'loss': ['mse'],
     'dcomp': [True],
     'normalize_image': [False],
@@ -23,28 +23,19 @@ params = [
   dict(use_mixed_precision=[False], model=['unet'], **base_params),
 ]
 
-run_ids = [
-    'ncpdnet_3d___i6_radial_stacks_mse_dcomp_1612291359',
-    'ncpdnet_3d___i6_spiral_stacks_mse_dcomp_1612291359',
-    'vnet_3d___radial_stacks_mse_dcomp_1612291357',
-    'vnet_3d___spiral_stacks_mse_dcomp_1612291357',
-]
 
-# eval_results = train_eval_grid(
-eval_results = eval_grid(
-    '3d_nc_eval',
-    # train_ncnet_multinet,
+eval_results = train_eval_grid(
+    '3d_nc',
+    train_ncnet_multinet,
     evaluate_nc_multinet,
     params,
     run_ids=run_ids,
-    # n_gpus_train=1,
-    # timeout_train=40,
-    # n_gpus_eval=1,
-    # n_samples_eval=100,
-    timeout=20,
-    n_gpus=1,
+    n_gpus_train=1,
+    timeout_train=40,
+    n_gpus_eval=1,
+    n_samples_eval=100,
     params_to_ignore=['use_mixed_precision', 'scale_factor'],
-    # checkpoints_train=7,
+    checkpoints_train=7,
     # resume_checkpoint=4,
     # resume_run_run_ids=run_ids,
     project='fastmri4',
