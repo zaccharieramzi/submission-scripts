f720c3b6c16b70f8dd7b2075235794bcce80df28
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-07-01 10:09:42 +0200

    corrected time for dip quali eval time

--- jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/review_evaluations/quali_dip_eval_time.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/review_evaluations/quali_dip_eval_time.py
@@ -2,7 +2,7 @@ from fastmri_recon.evaluate.scripts.dip_qualitative_validation import dip_qualit
 
 from jean_zay.submitit.general_submissions import get_executor
 
-executor = get_executor('dip_sc_time', timeout_hour=2, n_gpus=1, project='fastmri4')
+executor = get_executor('dip_sc_time', timeout_hour=5, n_gpus=1, project='fastmri4')
 job = executor.submit(
     dip_qualitative_validation,
     model_kwargs={'bn': True, 'n_up': 5},
