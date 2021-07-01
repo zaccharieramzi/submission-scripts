1e80782486ab1ddd5b3bff37548cf33c9c1844b5
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-07-01 10:16:30 +0200

    added dip multicoil sub script

---
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/dip_multicoil.py
@@ -0,0 +1,27 @@
+from fastmri_recon.evaluate.scripts.nc_dip_eval import evaluate_dip_nc as eval_fun
+
+from jean_zay.submitit.general_submissions import eval_grid
+
+base_params = {
+    # 'af': [4, 8],
+    'af': [4],
+    # 'acq_type': ['radial', 'spiral'],
+    'acq_type': ['radial'],
+    # 'contrast': ['CORPD_FBK', 'CORPDFS_FBK'],
+    'contrast': ['CORPD_FBK'],
+    'model_kwargs': [{'bn': True, 'n_up': 5}],
+    'multicoil': [True],
+}
+
+
+eval_results = eval_grid(
+    'dip_mc',
+    eval_fun,
+    base_params,
+    run_ids=None,
+    n_samples=1,
+    n_gpus=1,
+    timeout=20,
+    project='fastmri4',
+)
+print(eval_results)
