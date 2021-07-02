06afba7848a7563c842911dc2af3d41782d04e5a
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-07-02 16:15:11 +0200

    added dip brain quali eval sub script and corrected mc one

--- jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/review_evaluations/quali_eval_dip_mc_brain.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/review_evaluations/quali_eval_dip_mc_brain.py
@@ -3,20 +3,22 @@ from fastmri_recon.evaluate.scripts.dip_qualitative_validation import dip_qualit
 from jean_zay.submitit.general_submissions import get_executor
 
 
-zoom_box = [(180, 280), (100, 200)]
+zoom_box = [(200, 300), (100, 200)]
 
-executor = get_executor('dip_mc_quali', timeout_hour=2, n_gpus=1, project='fastmri4')
+executor = get_executor('dip_mc_brain_quali', timeout_hour=2, n_gpus=1, project='fastmri4')
 with executor.batch():
     for acq_type in ['radial', 'spiral']:
-        for af in [4, 8]:
+        for af in [4]:
             executor.submit(
                 dip_qualitative_validation,
                 model_kwargs={'bn': True, 'n_up': 5},
                 acq_type=acq_type,
                 af=af,
-                contrast='CORPDFS_FBK',
+                contrast=None,
+                slice_index=5,
                 n_iter=1000,
                 # zoom=zoom_box,
                 draw_zoom=zoom_box,
                 multicoil=True,
+                brain=True,
             )
