29cf91b5fba76b15772eeaecb0105f3444968d63
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-07-02 15:30:09 +0200

    made dip multicoil in eval mode

--- jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/dip_multicoil.py
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/dip_multicoil.py
@@ -3,12 +3,12 @@ from fastmri_recon.evaluate.scripts.nc_dip_eval import evaluate_dip_nc as eval_f
 from jean_zay.submitit.general_submissions import eval_grid
 
 base_params = {
-    # 'af': [4, 8],
-    'af': [4],
-    # 'acq_type': ['radial', 'spiral'],
-    'acq_type': ['radial'],
-    # 'contrast': ['CORPD_FBK', 'CORPDFS_FBK'],
-    'contrast': ['CORPD_FBK'],
+    'af': [4, 8],
+    # 'af': [4],
+    'acq_type': ['radial', 'spiral'],
+    # 'acq_type': ['radial'],
+    'contrast': ['CORPD_FBK', 'CORPDFS_FBK'],
+    # 'contrast': ['CORPD_FBK'],
     'model_kwargs': [{'bn': True, 'n_up': 5}],
     'multicoil': [True],
 }
@@ -19,9 +19,9 @@ eval_results = eval_grid(
     eval_fun,
     base_params,
     run_ids=None,
-    n_samples=1,
+    n_samples=65,
     n_gpus=1,
-    timeout=20,
+    timeout=100,
     project='fastmri4',
 )
 print(eval_results)
