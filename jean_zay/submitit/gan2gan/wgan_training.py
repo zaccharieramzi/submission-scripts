65c286ac2d1cc26f2aac6840ae408f1faa907a85
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-09-03 14:31:48 +0200

    added a gan2gan repo

---
+++ jean_zay/submitit/gan2gan/wgan_training.py
@@ -0,0 +1,13 @@
+from gan2gan.training.scripts.generator_training import wgan_training
+
+from jean_zay.submitit.general_submissions import get_executor
+
+
+executor = get_executor('wgan_training', timeout_hour=2, n_gpus=1, project='gan2gan')
+
+job = executor.submit(
+    wgan_training,
+    run_id='test',
+    epochs=1,
+    N=1,   
+)
