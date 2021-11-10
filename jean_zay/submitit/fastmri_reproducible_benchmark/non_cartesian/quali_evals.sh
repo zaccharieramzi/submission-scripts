10b9b84b4aed3ef480fff5a8647a632571c1867c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-11-10 16:58:01 +0100

    added a script to run all the qualitative evaluations I need for the revision

---
+++ jean_zay/submitit/fastmri_reproducible_benchmark/non_cartesian/quali_evals.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python quali_eval_ncsc.py
+python quali_eval_ncmc.py
+python quali_eval_ncmc_brain.py
+python quali_eval_ncmc_reverse.py
+python quali_eval_3d.py
+python review_evaluations/quali_eval_dip_sc.py
+python review_evaluations/quali_eval_dip_mc.py
+python review_evaluations/quali_eval_dip_mc_brain.py
