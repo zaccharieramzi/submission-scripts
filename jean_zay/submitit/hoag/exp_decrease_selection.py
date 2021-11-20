8baf67d8f45a95eb5f7bc8865851c8e48ca946f3
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-11-20 16:26:35 +0100

    reduced grid for best exp decrease search

--- jean_zay/submitit/hoag/exp_decrease_selection.py
+++ jean_zay/submitit/hoag/exp_decrease_selection.py
@@ -8,13 +8,13 @@ executor = get_executor('step_size_schedule', timeout_hour=2, n_gpus=1, project=
 
 jobs = []
 with executor.batch():
-    for exp_decrease in np.linspace(0.6, 0.9, 20):
+    for exp_decrease in np.linspace(0.78, 0.85, 20):
         job = executor.submit(
             get_alpha_for_exp_decrease,
             exp_decrease=exp_decrease,
         )
         jobs.append(job)
 
-for job, exp_decrease in zip(jobs, np.linspace(0.6, 0.9, 20)):
+for job, exp_decrease in zip(jobs, np.linspace(0.78, 0.85, 20)):
     alpha, val_losses = job.result()
     print(exp_decrease, alpha, min(val_losses))
