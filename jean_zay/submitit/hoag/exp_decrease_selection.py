27b628d9d989b8df1d60829c0adc9fae13509ffa
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-11-20 17:50:12 +0100

    made sure hoag uses the cpu executor

--- jean_zay/submitit/hoag/exp_decrease_selection.py
+++ jean_zay/submitit/hoag/exp_decrease_selection.py
@@ -1,10 +1,10 @@
 from hoag.scripts.best_exp_decrease import get_alpha_for_exp_decrease
 import numpy as np
 
-from jean_zay.submitit.general_submissions import get_executor
+from jean_zay.submitit.general_submissions import get_cpu_executor
 
 
-executor = get_executor('step_size_schedule', timeout_hour=2, n_gpus=1, project='hoag')
+executor = get_cpu_executor('step_size_schedule', timeout_hour=2, n_gpus=1, project='hoag')
 search_space = np.linspace(0.75, 0.85, 80)
 
 
