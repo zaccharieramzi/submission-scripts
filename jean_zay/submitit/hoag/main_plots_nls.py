2c7044c3c3f979a2b4c5deda4d2e76804a38347b
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-11-20 17:50:35 +0100

    added a paralle sub script for nls plots

---
+++ jean_zay/submitit/hoag/main_plots_nls.py
@@ -0,0 +1,27 @@
+from hoag.main_plots_nls import run_scheme
+import numpy as np
+
+from jean_zay.submitit.general_submissions import get_cpu_executor
+
+
+executor = get_cpu_executor('main_plots_nls_indep', timeout_hour=2, n_gpus=1, project='hoag')
+scheme_labels = ['shine-big-rank', 'fpn', 'original']
+
+
+jobs = []
+with executor.batch():
+    for scheme_label in scheme_labels:
+        job = executor.submit(
+            run_scheme,
+            scheme_label=scheme_label,
+        )
+        jobs.append(job)
+
+big_df_res = None
+for job, exp_decrease in zip(jobs, scheme_labels):
+    df_res = job.result()
+    if big_df_res is None:
+        big_df_res = df_res
+    else:
+        big_df_res = big_df_res.append(df_res)
+big_df_res.to_csv('plots_nls_results.csv')
