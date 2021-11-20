a9a1ee0a3faf75b904e4377cf7a99a3a21799e84
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-11-20 19:55:25 +0100

    changed main plots nls to run for multiple exp decrease factors

--- jean_zay/submitit/hoag/main_plots_nls.py
+++ jean_zay/submitit/hoag/main_plots_nls.py
@@ -4,24 +4,30 @@ import numpy as np
 from jean_zay.submitit.general_submissions import get_cpu_executor
 
 
-executor = get_cpu_executor('main_plots_nls_indep', timeout_hour=2, n_cpus=3, project='hoag')
+executor = get_cpu_executor('main_plots_nls_indep', timeout_hour=2, n_cpus=10, project='hoag')
 scheme_labels = ['shine-big-rank', 'fpn', 'original']
-
+search_space = np.linspace(0.75, 0.85, 10)
 
 jobs = []
 with executor.batch():
-    for scheme_label in scheme_labels:
-        job = executor.submit(
-            run_scheme,
-            scheme_label=scheme_label,
-        )
-        jobs.append(job)
+    for exponential_decrease_factor in search_space:
+        for scheme_label in scheme_labels:
+            job = executor.submit(
+                run_scheme,
+                scheme_label=scheme_label,
+                exponential_decrease_factor=exponential_decrease_factor,
+            )
+            jobs.append(job)
 
-big_df_res = None
-for job, exp_decrease in zip(jobs, scheme_labels):
-    df_res = job.result()
-    if big_df_res is None:
-        big_df_res = df_res
-    else:
-        big_df_res = big_df_res.append(df_res)
-big_df_res.to_csv('plots_nls_results.csv')
+job_counter = 0
+for exponential_decrease_factor in search_space:
+    big_df_res = None
+    for scheme_label in scheme_labels:
+        job = jobs[job_counter]
+        job_counter += 1
+        df_res = job.result()
+        if big_df_res is None:
+            big_df_res = df_res
+        else:
+            big_df_res = big_df_res.append(df_res)
+    big_df_res.to_csv('plots_nls_results_exp{exponential_decrease_factor}.csv')
