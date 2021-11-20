b382cb80573fd768aaeadb3ac30e46e35e88e354
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-11-20 17:14:24 +0100

    changed again exp decrease

--- jean_zay/submitit/hoag/exp_decrease_selection.py
+++ jean_zay/submitit/hoag/exp_decrease_selection.py
@@ -5,16 +5,19 @@ from jean_zay.submitit.general_submissions import get_executor
 
 
 executor = get_executor('step_size_schedule', timeout_hour=2, n_gpus=1, project='hoag')
+search_space = np.linspace(0.75, 0.85, 80)
+
 
 jobs = []
 with executor.batch():
-    for exp_decrease in np.linspace(0.78, 0.85, 20):
+    for exp_decrease in search_space:
         job = executor.submit(
             get_alpha_for_exp_decrease,
             exp_decrease=exp_decrease,
+            max_iter=100,
         )
         jobs.append(job)
 
-for job, exp_decrease in zip(jobs, np.linspace(0.78, 0.85, 20)):
+for job, exp_decrease in zip(jobs, search_space):
     alpha, val_losses = job.result()
     print(exp_decrease, alpha, min(val_losses))
