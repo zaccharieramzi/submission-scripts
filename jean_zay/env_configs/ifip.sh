d965a4e21d290a317a08e7ed6d5f47c2e4fb5061
zaccharieramzi <email@example.com>
2022-01-04 14:52:52 +0100

    added an offline mode to wandb for ifip in order to still be able to run wandb on jz

--- jean_zay/env_configs/ifip.sh
+++ jean_zay/env_configs/ifip.sh
@@ -6,3 +6,4 @@ export CHKPT_PATH=$SCRATCH/checkpoints/ifip/
 export LOG_PATH=$SCRATCH/logs/ifip/
 export KERAS_HOME=$SCRATCH/.keras/
 export WANDB_PATH=$SCRATCH/wandb/ifip/
+export WANDB_MODE=offline
