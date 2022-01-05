d02d50a77e6ddff86d9898e61d3e096b5eac2b42
zaccharieramzi <email@example.com>
2022-01-05 15:28:08 +0100

    added new resolvers and changed name of Search path plugin

--- jean_zay/hydra_config/searchpath_config.py
+++ jean_zay/hydra_config/searchpath_config.py
@@ -1,9 +1,21 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from hydra.core.config_search_path import ConfigSearchPath
 from hydra.plugins.search_path_plugin import SearchPathPlugin
+from omegaconf import OmegaConf
 
+def qos_from_hours(hours):
+    if hours > 20:
+        qos = 't4'
+    elif hours > 2:
+        qos = 't3'
+    else:
+        qos = 'dev'
+    return qos
 
-class ExampleSearchPathPlugin(SearchPathPlugin):
+OmegaConf.register_new_resolver('multiply10', lambda x: x * 10)
+OmegaConf.register_new_resolver('qos_from_hours', qos_from_hours)
+
+class JeanZaySearchPathPlugin(SearchPathPlugin):
     def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
         # Appends the search path for this plugin to the end of the search path
         # Note that foobar/conf is outside of the example plugin module.
