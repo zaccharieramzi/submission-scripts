8991c3864d7a5aa11203cd83a631d14d418ec449
zaccharieramzi <email@example.com>
2022-01-05 19:24:13 +0100

    better presentation for debug cifar hydra

--- jean_zay/raw/ifip/debug_train_cifar.sh
+++ jean_zay/raw/ifip/debug_train_cifar.sh
@@ -2,4 +2,7 @@ cd $WORK/implicit-fields-inverse-problems
 python ifip/training/train_cifar.py -m hydra/launcher=dev\
     'hydra.searchpath=[pkg://jean_zay/hydra_config]'\
     training.fit.epochs=1 note=debugging\
-    '+compile={lr_latent:5e-4,lr_mlp:1e-5},{lr_latent:1e-5,lr_mlp:5e-6}'
+    '+compile=\
+        {lr_latent:5e-4,lr_mlp:1e-5},\
+        {lr_latent:1e-5,lr_mlp:5e-6}\
+    '
