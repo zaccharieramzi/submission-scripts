a329442437213d4301d4c1a9598243f15426646f
zaccharieramzi <email@example.com>
2022-01-05 19:08:28 +0100

    corrected bug in debug script

--- jean_zay/raw/ifip/debug_train_cifar.sh
+++ jean_zay/raw/ifip/debug_train_cifar.sh
@@ -1,5 +1,5 @@
 cd $WORK/implicit-fields-inverse-problems
-python ifip/training/train_cifar.py -i -m hydra/launcher=dev\\
-    'hydra.searchpath=[pkg://jean_zay/hydra_config]'\\
-    training.fit.epochs=1 note=debugging \\
+python ifip/training/train_cifar.py -i -m hydra/launcher=dev\
+    'hydra.searchpath=[pkg://jean_zay/hydra_config]'\
+    training.fit.epochs=1 note=debugging\
     '+compile={lr_latent:5e-4,lr_mlp:1e-5}'
