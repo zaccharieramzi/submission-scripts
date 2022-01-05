b0a80b39516991b153e46cd5a26dee8b9e78d08a
zaccharieramzi <email@example.com>
2022-01-05 19:43:56 +0100

    added double quotes for the debug to work

--- jean_zay/raw/ifip/debug_train_cifar.sh
+++ jean_zay/raw/ifip/debug_train_cifar.sh
@@ -10,4 +10,4 @@ cd $WORK/implicit-fields-inverse-problems
 python ifip/training/train_cifar.py -m hydra/launcher=dev\
     'hydra.searchpath=[pkg://jean_zay/hydra_config]'\
     training.fit.epochs=1 note=debugging\
-    +compile=${params[*]}
+    "+compile=${params[*]}"
