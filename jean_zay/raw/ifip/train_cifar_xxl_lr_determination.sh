9065b40ce14194004682f9b39399e965885a017f
zaccharieramzi <email@example.com>
2022-01-06 16:22:44 +0100

    corrected callback model name

--- jean_zay/raw/ifip/train_cifar_xxl_lr_determination.sh
+++ jean_zay/raw/ifip/train_cifar_xxl_lr_determination.sh
@@ -14,5 +14,5 @@ cd $WORK/implicit-fields-inverse-problems
 python ifip/training/train_cifar.py -m hydra/launcher=4gpus_dev\
     'hydra.searchpath=[pkg://jean_zay/hydra_config]'\
     +model=xxl training.fit.epochs=5\
-    '+callbacks.model.name=ifip_xxl_lrl${compile.lr_latent}_lrm${compile.lr_mlp}'\
+    'callbacks.model.name=ifip_xxl_lrl${compile.lr_latent}_lrm${compile.lr_mlp}'\
     "+compile=${params[*]}"
