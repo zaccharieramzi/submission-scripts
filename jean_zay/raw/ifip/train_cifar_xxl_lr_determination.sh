0e21fcfdbddf118b7708910cd34e94065ce7fef6
zaccharieramzi <email@example.com>
2022-01-06 12:32:46 +0100

    added more param config in lr determination

--- jean_zay/raw/ifip/train_cifar_xxl_lr_determination.sh
+++ jean_zay/raw/ifip/train_cifar_xxl_lr_determination.sh
@@ -2,12 +2,14 @@
 
 IFS=''
 params=(
+    '{lr_latent:1e-3,lr_mlp:1e-5},'
     '{lr_latent:5e-4,lr_mlp:1e-5},'
+    '{lr_latent:5e-4,lr_mlp:5e-6},'
     '{lr_latent:1e-5,lr_mlp:5e-6}'
 )
 
 cd $WORK/implicit-fields-inverse-problems
 python ifip/training/train_cifar.py -m hydra/launcher=4gpus_dev\
     'hydra.searchpath=[pkg://jean_zay/hydra_config]'\
-    +model=xxl training.fit.epochs=20\
+    +model=xxl training.fit.epochs=5\
     "+compile=${params[*]}"
