1d6d199b72430672ceebbfa3afade5639bc29594
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-22 09:53:36 +0200

    added pretraining scripts for cifar mdeq

---
+++ jean_zay/submitit/shine/cifar_large_pretrain.py
@@ -0,0 +1,33 @@
+from mdeq_lib.training.cls_train import train_classifier
+
+from jean_zay.submitit.general_submissions import train_eval_grid
+
+
+job_name = 'shine_classifier_cifar_large_pretrain'
+n_gpus = 4
+params = dict(
+    model_size='LARGE',
+    dataset='cifar',
+    n_gpus=n_gpus,
+    n_epochs=220,
+    save_at=50,
+    seed=0,
+)
+
+def _dummy_eval(**kwargs):
+    pass
+
+train_eval_grid(
+    job_name,
+    train_classifier,
+    _dummy_eval,
+    [params],
+    to_grid=False,
+    timeout_train=4,
+    n_gpus_train=n_gpus,
+    timeout_eval=0,
+    n_gpus_eval=0,
+    project='shine',
+    params_to_ignore=['n_epochs', 'save_at', 'restart_from'],
+    torch=True,
+)
---
+++ jean_zay/submitit/shine/cifar_tiny_pretrain.py
@@ -0,0 +1,33 @@
+from mdeq_lib.training.cls_train import train_classifier
+
+from jean_zay.submitit.general_submissions import train_eval_grid
+
+
+job_name = 'shine_classifier_cifar_tiny_pretrain'
+n_gpus = 4
+params = dict(
+    model_size='TINY',
+    dataset='cifar',
+    n_gpus=n_gpus,
+    n_epochs=80,
+    save_at=14,
+    seed=0,
+)
+
+def _dummy_eval(**kwargs):
+    pass
+
+train_eval_grid(
+    job_name,
+    train_classifier,
+    _dummy_eval,
+    [params],
+    to_grid=False,
+    timeout_train=1,
+    n_gpus_train=n_gpus,
+    timeout_eval=0,
+    n_gpus_eval=0,
+    project='shine',
+    params_to_ignore=['n_epochs', 'save_at', 'restart_from'],
+    torch=True,
+)
