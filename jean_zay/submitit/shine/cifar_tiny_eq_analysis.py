73720960d2f4d1c8de731284d66c7482b51c0da6
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-26 10:57:24 +0200

    added a sub script for cifar tiny eq analysis

---
+++ jean_zay/submitit/shine/cifar_tiny_eq_analysis.py
@@ -0,0 +1,27 @@
+from mdeq_lib.debug.eq_init_analysis import analyze_equilibrium_initialization
+
+from jean_zay.submitit.general_submissions import get_executor, ParameterGrid
+
+
+job_name = 'shine_eq_analy_cifar_tiny'
+n_gpus = 2
+
+exec = get_executor(job_name, n_gpus, timeout_hour=2, project='shine', no_force_32=True)
+
+params = dict(
+    model_size='TINY',
+    dataset='cifar',
+    n_samples_train=[64*5, 64*10, 50_000],
+    n_images=100,
+    checkpoint=[14, 40, 60, 70],
+    on_cpu=False,
+    n_gpus=n_gpus,
+)
+params = list(ParameterGrid(params))
+
+jobs = []
+with exec.batch():
+    for param in params:
+        job = exec.submit(analyze_equilibrium_initialization, **param)
+        jobs.append(job)
+df_res_list = [job.result() for job in jobs]
