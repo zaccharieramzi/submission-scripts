8c326d4dbbb7ca36dad37852110ff0ed8340b234
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-05-06 10:34:41 +0200

    added an evaluation at the end of cifar pretraining

--- jean_zay/submitit/shine/cifar_large_reg_pretrain.py
+++ jean_zay/submitit/shine/cifar_large_reg_pretrain.py
@@ -1,3 +1,4 @@
+from mdeq_lib.evaluate.cls_valid import evaluate_classifier
 from mdeq_lib.training.cls_train import train_classifier
 
 from jean_zay.submitit.general_submissions import train_eval_grid
@@ -5,6 +6,7 @@ from jean_zay.submitit.general_submissions import train_eval_grid
 
 job_name = 'shine_classifier_cifar_large_pretrain'
 n_gpus = 1
+
 params = dict(
     model_size='LARGE_reg',
     dataset='cifar',
@@ -20,13 +22,13 @@ def _dummy_eval(**kwargs):
 train_eval_grid(
     job_name,
     train_classifier,
-    _dummy_eval,
+    evaluate_classifier,
     [params],
     to_grid=False,
     timeout_train=20,
     n_gpus_train=n_gpus,
-    timeout_eval=0,
-    n_gpus_eval=0,
+    timeout_eval=2,
+    n_gpus_eval=n_gpus,
     project='shine',
     params_to_ignore=['n_epochs', 'save_at', 'restart_from'],
     torch=True,
