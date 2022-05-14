267736c13fa2d10c7f5a0f3011d32ca90d23c27e
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-05-14 14:37:11 +0200

    corrected merge script

--- jean_zay/raw/benchopt/merge_restricted_benches.sh
+++ jean_zay/raw/benchopt/merge_restricted_benches.sh
@@ -10,9 +10,9 @@ cd $WORK/benchmark_resnet_classif
 for dataset in {'mnist',}; do
     for with_validation in {'True', 'False'}; do
         BASIC_CMD="benchopt run ."
-        BASIC_CMD="$BASIC_CMD -o *18 -d $dataset[*,with_validation=$with_validation] -r 1 -n 200 --timeout 10800"
+        BASIC_CMD="$BASIC_CMD -o *18 -d ${dataset}[*,with_validation=${with_validation}] -r 1 -n 200 --timeout 10800"
         args=''
-        if [ "$with_validation" == "True" ]; then
+        if [ "${with_validation}" == "True" ]; then
             model="sgd-torch[batch_size=128"
             args="${args} -s ${model},data_aug=True,*,lr_schedule=cosine,momentum=0.9,nesterov=False,weight_decay=0.0005]"
             args="${args} -s ${model},data_aug=True,*,lr_schedule=step,momentum=0.9,nesterov=False,weight_decay=0.0005]"
@@ -35,7 +35,7 @@ for dataset in {'mnist',}; do
             # sgd with data aug + momentum + step + wd
             args="${args} -s ${model},data_aug=True,*,lr_schedule=step,momentum=0.9,nesterov=False,weight_decay=0.0005]"
             # SGD TF
-            if [ "$dataset" != "mnist" ]; then
+            if [ "${dataset}" != "mnist" ]; then
                 model="sgd-tf[batch_size=128"
                 # sgd with data aug + momentum + cosine + wd
                 args="${args} -s ${model},data_aug=True,*,lr_schedule=cosine,momentum=0.9,nesterov=False,weight_decay=0.0005]"
