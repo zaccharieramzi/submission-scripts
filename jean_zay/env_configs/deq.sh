1b0953cc7c9ef8182a56256b6d4c17356cee9a15
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-05-25 00:22:40 +0200

    added a deq env config

---
+++ jean_zay/env_configs/deq.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+module purge
+module load pytorch-gpu/py3/1.6.0
+
+export FASTMRI_DATA_DIR=$SCRATCH/
+export IMAGENET_DIR=$SCRATCH/imagenet/
+export CIFAR_DIR=$SCRATCH/cifar/
+export WIKITEXT_DIR=$SCRATCH/wikitext-103/
+export TMP_DIR=$JOBSCRATCH/
+export SHINE_LOGS=$SCRATCH/logs
+export SHINE_CHECKPOINTS=$SCRATCH/checkpoints
+export SHINE_DATA=$SCRATCH/
+export SHINE_CONFIG=$WORK/shine/experiments/
+export WORK_DIR=$WORK/shine/
