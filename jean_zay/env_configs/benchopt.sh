11c9e037fd25cc34c517154b3a7ea65cce27c660
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2022-07-27 10:53:22 +0200

    added malloc fix in benchopt env config

--- jean_zay/env_configs/benchopt.sh
+++ jean_zay/env_configs/benchopt.sh
@@ -4,5 +4,6 @@ export PYTHONUSERBASE=$WORK/.local_torch
 module load pytorch-gpu/py3/1.10.1
 export PATH=$WORK/.local_torch/bin:$PATH
 export TMPDIR=$JOBSCRATCH
+export MALLOC_TRIM_THRESHOLD_=0
 
 cd $WORK/benchmark_resnet_classif
