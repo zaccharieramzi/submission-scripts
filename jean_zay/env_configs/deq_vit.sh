c3dbdb0eafc1e2563e4d189588b19f0f43321520
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2023-06-01 16:07:42 +0200

    tried fix on ld library

--- jean_zay/env_configs/deq_vit.sh
+++ jean_zay/env_configs/deq_vit.sh
@@ -5,3 +5,4 @@ module load cuda/11.7.1
 module load cudnn/8.5.0.96-11.7-cuda
 
 cd $xpa_ALL_CCFRWORK/deq-vit
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfslocalsup/pub/anaconda-py3/2021.05/envs/python-3.10.4/lib
